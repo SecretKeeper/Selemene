977f28ecf3b61c230f4bb3b9e2cf1faab30f43f5
TeamOF <shervin.ourfamily@gmail.com>
2021-05-02 00:47:53 +0430

    creating

--- app/src/main/java/net/teamof/whisper/MainActivity.kt
+++ app/src/main/java/net/teamof/whisper/MainActivity.kt
@@ -4,20 +4,8 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.ExperimentalFoundationApi
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.*
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.navigation.NavHostController
-import androidx.navigation.NavType
-import androidx.navigation.compose.*
-import net.teamof.whisper.components.BottomAppBar
-import net.teamof.whisper.components.FloatingActionButton
 import net.teamof.whisper.screens.*
-import net.teamof.whisper.ui.theme.WhisperTheme
 
 
 class MainActivity : ComponentActivity() {
