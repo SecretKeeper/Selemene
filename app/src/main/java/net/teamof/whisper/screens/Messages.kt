2ac24be86b00d33ccd6ea737373eecfd5b3ccb86
TeamOF <shervin.ourfamily@gmail.com>
2021-06-08 18:41:53 +0430

    creating

--- app/src/main/java/net/teamof/whisper/screens/Messages.kt
+++ app/src/main/java/net/teamof/whisper/screens/Messages.kt
@@ -1,9 +1,12 @@
 package net.teamof.whisper.screens
 
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import net.teamof.whisper.components.MessagePortal
 import net.teamof.whisper.models.MessagePortal
@@ -14,7 +17,7 @@ fun Messages(
     navController: NavController,
     messages: List<MessagePortal>
 ) {
-    LazyColumn {
+    LazyColumn(Modifier.padding(bottom = 70.dp)) {
         itemsIndexed(messages) { _, item ->
             MessagePortal(item, navController)
         }
