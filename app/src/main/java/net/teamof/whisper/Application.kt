b207b2b4cab1bdbef343398f16896c4eea8f58ff
TeamOF <shervin.ourfamily@gmail.com>
2021-06-13 16:05:24 +0430

    implemented ObjectBox Browser

--- app/src/main/java/net/teamof/whisper/Application.kt
+++ app/src/main/java/net/teamof/whisper/Application.kt
@@ -1,12 +1,19 @@
 package net.teamof.whisper
 
 import android.app.Application
+import android.util.Log
 import dagger.hilt.android.HiltAndroidApp
+import io.objectbox.android.AndroidObjectBrowser
 
 @HiltAndroidApp
 class Whisper : Application() {
     override fun onCreate() {
         super.onCreate()
         ObjectBox.init(this)
+        
+        if (BuildConfig.DEBUG) {
+            val started = AndroidObjectBrowser(ObjectBox.store).start(this)
+            Log.i("ObjectBrowser", "Started: $started")
+        }
     }
 }
