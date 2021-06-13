99d63691ba7cc0fd14637f2bd97b5d2340ee7ccf
TeamOF <shervin.ourfamily@gmail.com>
2021-06-13 13:21:13 +0430

    initialized @HiltAndroidApp

--- app/src/main/java/net/teamof/whisper/Application.kt
+++ app/src/main/java/net/teamof/whisper/Application.kt
@@ -1,7 +1,9 @@
 package net.teamof.whisper
 
 import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
 
+@HiltAndroidApp
 class Whisper : Application() {
     override fun onCreate() {
         super.onCreate()
