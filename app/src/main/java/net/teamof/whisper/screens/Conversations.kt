4300fa85cec6fa69e8949e404049fe82a1e74793
TeamOF <shervin.ourfamily@gmail.com>
2021-06-14 17:35:20 +0430

    moved conversations observer to it owns composable

--- app/src/main/java/net/teamof/whisper/screens/Conversations.kt
+++ app/src/main/java/net/teamof/whisper/screens/Conversations.kt
@@ -6,18 +6,27 @@ import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
 import net.teamof.whisper.components.Conversation
 import net.teamof.whisper.models.Conversation
+import net.teamof.whisper.viewModel.ConversationsViewModel
 
 @ExperimentalMaterialApi
 @Composable
 fun Conversations(
     navController: NavController,
-    conversations: List<Conversation>
+    conversationsViewModel: ConversationsViewModel = viewModel(),
 ) {
+
+    val conversations: List<Conversation> by conversationsViewModel.conversations.observeAsState(
+        listOf()
+    )
+
     Column(
         Modifier
             .padding(bottom = 70.dp)
