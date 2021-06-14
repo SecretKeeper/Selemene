412c7f8762bef2505abf68620002bf7e8717b91f
TeamOF <shervin.ourfamily@gmail.com>
2021-06-14 12:58:23 +0430

    added createConversation deleteConversation(s) methods

--- app/src/main/java/net/teamof/whisper/viewModel/ConversationsViewModel.kt
+++ app/src/main/java/net/teamof/whisper/viewModel/ConversationsViewModel.kt
@@ -5,6 +5,7 @@ import androidx.lifecycle.ViewModel
 import io.objectbox.Box
 import net.teamof.whisper.ObjectBox
 import net.teamof.whisper.models.Conversation
+import net.teamof.whisper.models.Conversation_
 
 class ConversationsViewModel : ViewModel() {
 
@@ -12,17 +13,47 @@ class ConversationsViewModel : ViewModel() {
         ObjectBox.store.boxFor(Conversation::class.java)
 
     private val _conversations: MutableLiveData<MutableList<Conversation>> by lazy {
-        MutableLiveData<MutableList<Conversation>>(loadMessages())
+        MutableLiveData<MutableList<Conversation>>(loadConversations())
     }
 
     val conversations: MutableLiveData<MutableList<Conversation>> = _conversations
 
-    private fun loadMessages(): MutableList<Conversation> {
+    private fun loadConversations(): MutableList<Conversation> {
         return conversationBox.all
     }
 
-    fun createConversation(conversation: Conversation) {
-        conversationBox.put(conversation)
+    private fun isConversationExist(conversation: Conversation): Long {
+        val query =
+            conversationBox.query().equal(Conversation_.channel, conversation.channel).build()
+        val result = query.count()
+        query.close();
+
+        return result
     }
 
+    fun getConversation(channel: String): Conversation? {
+        val query = conversationBox.query().equal(Conversation_.channel, channel).build()
+        val result = query.findFirst()
+        query.close()
+
+        return result
+    }
+
+    fun createConversation(conversation: Conversation) {
+
+        if (isConversationExist(conversation) == 0L) {
+            conversationBox.put(conversation)
+            loadConversations()
+        }
+
+    }
+
+    fun deleteConversation() {
+        conversationBox.removeAll()
+        loadConversations()
+    }
+
+    fun deleteConversation(conversation: Array<Conversation>) {
+
+    }
 }
