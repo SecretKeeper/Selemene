df429cdd44e98caff63e0066754bea2db6e7a582
TeamOF <shervin.ourfamily@gmail.com>
2021-06-14 12:33:58 +0430

    implemented create conversation action

--- app/src/main/java/net/teamof/whisper/components/Contact.kt
+++ app/src/main/java/net/teamof/whisper/components/Contact.kt
@@ -1,14 +1,11 @@
 package net.teamof.whisper.components
 
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.Card
-import androidx.compose.material.Icon
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
@@ -19,20 +16,56 @@ import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.NavController
 import coil.transform.CircleCropTransformation
 import com.google.accompanist.coil.rememberCoilPainter
 import net.teamof.whisper.R
 import net.teamof.whisper.models.Contact
+import net.teamof.whisper.models.Conversation
+import net.teamof.whisper.viewModel.ConversationsViewModel
+import net.teamof.whisper.viewModel.UserViewModel
+import java.util.*
 
+@ExperimentalMaterialApi
 @Composable
-fun Contact(data: Contact, action: String) {
+fun Contact(
+    navController: NavController,
+    data: Contact,
+    action: String,
+    userViewModel: UserViewModel = viewModel(),
+    conversationsViewModel: ConversationsViewModel = viewModel()
+) {
 
     var checked by rememberSaveable(data.selected) { mutableStateOf(data.selected) }
+    val currentUserId = userViewModel.userId.observeAsState()
 
-    Card(elevation = 0.dp, modifier = Modifier
-        .fillMaxWidth()
-        .padding(start = 40.dp)
-        .clickable { checked = !checked }) {
+    Card(
+        elevation = 0.dp,
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(start = 40.dp),
+        onClick = {
+            when (action) {
+                "CreateGroup" -> checked = !checked
+                "Messaging" -> {
+                    val channelUUID: String = UUID.nameUUIDFromBytes(
+                        (currentUserId.value.toString() + data.user_id).toByteArray()
+                    ).toString()
+
+                    conversationsViewModel.createConversation(
+                        Conversation(
+                            channel = channelUUID,
+                            user_id = data.user_id,
+                            username = data.username,
+                            user_image = data.user_image,
+                        )
+                    )
+
+                    navController.navigate("Messaging/$channelUUID")
+                }
+            }
+        }) {
         Row(
             verticalAlignment = Alignment.CenterVertically,
             modifier = Modifier
@@ -40,7 +73,7 @@ fun Contact(data: Contact, action: String) {
                 .padding(vertical = 10.dp, horizontal = 10.dp)
         ) {
             Image(
-                painter = rememberCoilPainter(request = data.image,
+                painter = rememberCoilPainter(request = data.user_image,
                     requestBuilder = {
                         transformations(CircleCropTransformation())
                     }
@@ -63,7 +96,7 @@ fun Contact(data: Contact, action: String) {
                             .padding(bottom = 7.dp)
                             .weight(1f)
                     )
-                    if (action == "CreateGroup")
+                    if (action == "Messaging")
                         if (checked) Icon(
                             imageVector = ImageVector.vectorResource(id = R.drawable.ic_checkmark),
                             contentDescription = null,
