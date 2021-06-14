e651ba05f42e17b74ed2442218502c1e83cb2f83
TeamOF <shervin.ourfamily@gmail.com>
2021-06-14 13:16:06 +0430

    make channel unique and indexed , also default set default value for fields based ObjectBox docs

--- app/src/main/java/net/teamof/whisper/models/Conversation.kt
+++ app/src/main/java/net/teamof/whisper/models/Conversation.kt
@@ -2,15 +2,18 @@ package net.teamof.whisper.models
 
 import io.objectbox.annotation.Entity
 import io.objectbox.annotation.Id
+import io.objectbox.annotation.Index
+import io.objectbox.annotation.Unique
 
 @Entity
 data class Conversation(
     @Id
     var id: Long = 0,
-    val user_id: Long,
+    @Unique @Index var channel: String,
+    var user_id: Long = 0,
     val username: String,
     val user_image: String,
-    val last_message: String,
-    val last_message_time: String,
-    val unread_messages: Int
+    val last_message: String = "There is no messages",
+    val last_message_time: String = "",
+    val unread_messages: Int = 0
 )
