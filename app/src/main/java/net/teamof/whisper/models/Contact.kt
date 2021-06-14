025acf5e74a0e3470a10223625ff5a99f5016449
TeamOF <shervin.ourfamily@gmail.com>
2021-06-14 12:36:39 +0430

    make Contact.kt as ObjectBox entity

--- app/src/main/java/net/teamof/whisper/models/Contact.kt
+++ app/src/main/java/net/teamof/whisper/models/Contact.kt
@@ -1,9 +1,17 @@
 package net.teamof.whisper.models
 
+import io.objectbox.annotation.Entity
+import io.objectbox.annotation.Id
+import io.objectbox.annotation.Index
+import io.objectbox.annotation.Unique
+
+@Entity
 data class Contact(
-    val user_id: Int,
+    @Id
+    var id: Long = 0,
+    @Index @Unique val user_id: Long,
     val username: String,
-    val image: String,
+    val user_image: String,
     val status: String,
     val is_online: Boolean,
     var selected: Boolean
