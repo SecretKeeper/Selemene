5f22b0eedba9c11d1dc37a0f2949f744487cab54
TeamOF <shervin.ourfamily@gmail.com>
2021-06-14 12:56:22 +0430

    fix sample images URIs and implemented navigation

--- app/src/main/java/net/teamof/whisper/screens/Contacts.kt
+++ app/src/main/java/net/teamof/whisper/screens/Contacts.kt
@@ -2,12 +2,10 @@ package net.teamof.whisper.screens
 
 import android.annotation.SuppressLint
 import androidx.compose.foundation.ExperimentalFoundationApi
-import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -18,6 +16,7 @@ import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.navigation.NavController
 import net.teamof.whisper.R
 import net.teamof.whisper.components.Contact
 import net.teamof.whisper.models.Contact
@@ -25,132 +24,117 @@ import net.teamof.whisper.models.Contact
 val contacts = listOf(
     Contact(
         1,
+        1,
         "Jaina Proudmoore",
         "https://c4.wallpaperflare.com/wallpaper/607/463/825/world-of-warcraft-jaina-proudmoore-magic-mazert-young-turquoise-hd-wallpaper-preview.jpg",
-        " Lord Admiral, ruler of the Kul Tiras kingdom",
-        true,
-        true
+        " Lord Admiral, ruler of the Kul Tiras kingdom"
     ),
     Contact(
+        2,
         3,
         "Lichking",
         "https://million-wallpapers.com/wallpapers/1/65/16175274567205625182.jpg",
-        "Your soul is mine",
-        true,
-        false
+        "Your soul is mine"
     ),
     Contact(
+        3,
         4,
         "Thrall",
-        "https://image3.uhdpaper.com/wallpaper-hd/thrall-world-of-warcraft-battle-for-azeroth-orc-uhdpaper.com-hd-3.978.jpg",
-        "Lord of the Clans",
-        true,
-        false
+        "https://cdn.hearthstonetopdecks.com/wp-content/uploads/2014/04/thrall-shaman001-640x1136.jpg",
+        "Lord of the Clans"
     ),
     Contact(
         5,
-        "Thrall",
-        "https://image3.uhdpaper.com/wallpaper-hd/thrall-world-of-warcraft-battle-for-azeroth-orc-uhdpaper.com-hd-3.978.jpg",
-        "Lord of the Clans",
-        true,
-        false
+        6,
+        "Sylvanas Windrunner",
+        "https://wallpaperchat.com/walls/full/2/2/5/402192.jpg",
+        "Dark Lady, Also known Banshee Queen"
     ),
     Contact(
         6,
-        "Sylvanas Windrunner",
-        "https://image3.uhdpaper.com/wallpaper-hd/thrall-world-of-warcraft-battle-for-azeroth-orc-uhdpaper.com-hd-3.978.jpg",
-        "Dark Lady, Also known Banshee Queen",
-        true,
-        false
+        7,
+        "Anduin Wrynn",
+        "https://mocah.org/thumbs/329166-WoW-The-Alliance-Anduin-Wrynn-Genn-Greymane-4K-iphone-wallpaper.jpg",
+        "The King of the Stormwind City"
     ),
     Contact(
         7,
-        "Anduin Wrynn",
-        "https://c4.wallpaperflare.com/wallpaper/521/133/218/alliance-anduin-wrynn-armour-blonde-wallpaper-preview.jpg",
-        "The King of the Stormwind City",
-        true,
-        false
+        8,
+        "Illidan Stormrage",
+        "https://images.wallpapersden.com/image/download/illidan-stormrage_a2xuaGeUmZqaraWkpJRsamWtZmhoaQ.jpg",
+        "Lord of the Outland"
     ),
     Contact(
         8,
-        "Illidan Stormrage",
-        "https://c4.wallpaperflare.com/wallpaper/805/742/532/warcraft-world-of-warcraft-demon-illidan-stormrage-night-elf-hd-wallpaper-preview.jpg",
-        "Lord of the Outland",
-        true,
-        false
-    ),
-    Contact(
         9,
         "Tyrande Whisperwind",
         "https://coolwallpapers.me/picsup/2990861-elves-blue-hair-fantasy-art-world-of-warcraft-tyrande-whisperwind___mixed-wallpapers.jpg",
-        "High Priestess of Elune",
-        true,
-        false
+        "High Priestess of Elune"
     ),
     Contact(
+        9,
         10,
         "Garrosh Hellscream",
         "https://i.pinimg.com/originals/28/a3/b7/28a3b7ced21793e17d1b9f5d5f1f3fd0.png",
-        "Warlord of the Warsong clan",
-        true,
-        false
+        "Warlord of the Warsong clan"
+    ),
+    Contact(
+        10,
+        11,
+        "Kel'thuzad",
+        "https://www.mobygames.com/images/promo/original/1465476647-1936338714.jpg",
+        "Founder of the Cult of the Damned"
     ),
     Contact(
         11,
-        "Kel'thuzad",
-        " https://i.ytimg.com/vi/uNeMsENbeJw/hqdefault.jpg",
-        "Founder of the Cult of the Damned",
-        true,
-        false
-    ),
-    Contact(
         12,
         "Liadrin",
         "https://i.pinimg.com/originals/e1/81/25/e181259a1cf944de6dd8cb4d40142185.jpg",
-        "Blood Knight Matriarch",
-        true,
-        false
+        "Blood Knight Matriarch"
     ),
 )
 
+@ExperimentalMaterialApi
 @SuppressLint("UnrememberedMutableState")
 @OptIn(ExperimentalFoundationApi::class)
 @Composable
-fun Contacts(action: String) {
+fun Contacts(navController: NavController, action: String) {
 
     val grouped = contacts.groupBy { it.username[0] }
 
-    Column(modifier = Modifier.padding(20.dp)) {
-        Row(verticalAlignment = Alignment.CenterVertically) {
-            Text(text = "Choose users to invite", modifier = Modifier.weight(1f))
-            TextButton(
-                shape = RoundedCornerShape(10.dp),
-                onClick = { /* Do something! */ },
+    LazyColumn {
+
+        if (action == "CreateGroup")
+            item {
+                Column(modifier = Modifier.padding(20.dp)) {
+                    Row(verticalAlignment = Alignment.CenterVertically) {
+                        Text(text = "Choose users to invite", modifier = Modifier.weight(1f))
+                        TextButton(
+                            shape = RoundedCornerShape(10.dp),
+                            onClick = { /* Do something! */ },
 //                colors = ButtonDefaults.buttonColors(
 //                    backgroundColor = MaterialTheme.colors.primary
 //                )
-            ) {
-                Icon(
-                    imageVector = ImageVector.vectorResource(id = R.drawable.ic_checkmark),
-                    tint =  MaterialTheme.colors.primary,
-                    contentDescription = null,
-                    modifier = Modifier
-                        .width(18.dp)
-                        .height(18.dp)
-                )
-                Text(
-                    "Done",
-                    color =  MaterialTheme.colors.primary,
-                    fontSize = 14.sp,
-                    fontWeight = FontWeight.W600,
-                    modifier = Modifier.padding(start = 7.dp)
-                )
+                        ) {
+                            Icon(
+                                imageVector = ImageVector.vectorResource(id = R.drawable.ic_checkmark),
+                                tint = MaterialTheme.colors.primary,
+                                contentDescription = null,
+                                modifier = Modifier
+                                    .width(18.dp)
+                                    .height(18.dp)
+                            )
+                            Text(
+                                "Done",
+                                color = MaterialTheme.colors.primary,
+                                fontSize = 14.sp,
+                                fontWeight = FontWeight.W600,
+                                modifier = Modifier.padding(start = 7.dp)
+                            )
+                        }
+                    }
+                }
             }
-        }
-    }
-
-
-    LazyColumn(modifier = Modifier.padding(top = 80.dp)) {
 
         item {
             TextField(
@@ -179,7 +163,7 @@ fun Contacts(action: String) {
             }
 
             itemsIndexed(contactsForInitial) { _, contact ->
-                Contact(contact, action)
+                Contact(navController, contact, action)
             }
         }
     }
