c85620bcf61403faf8ccfb321587051175111fbc
TeamOF <shervin.ourfamily@gmail.com>
2021-06-15 12:59:45 +0430

    redesigned Conversation.kt

--- app/src/main/java/net/teamof/whisper/components/Conversation.kt
+++ app/src/main/java/net/teamof/whisper/components/Conversation.kt
@@ -7,6 +7,8 @@ import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
@@ -17,15 +19,22 @@ import coil.transform.CircleCropTransformation
 import com.google.accompanist.coil.rememberCoilPainter
 import net.teamof.whisper.models.Conversation
 import net.teamof.whisper.ui.theme.fontFamily
+import net.teamof.whisper.viewModel.MessagesViewModel
+import org.ocpsoft.prettytime.PrettyTime
 
 @ExperimentalMaterialApi
 @Composable
 fun Conversation(
-    data: Conversation,
+    conversation: Conversation,
     navController: NavController
 ) {
+
+    val cachedConversation = remember(conversation) { mutableStateOf(conversation) }
+    val messagesViewModel = MessagesViewModel(conversation.channel)
+    val lastMessage = messagesViewModel.getLastMessage()
+
     Card(
-        onClick = { navController.navigate("Messaging/${data.channel}") },
+        onClick = { navController.navigate("Messaging/${cachedConversation.value.channel}") },
         modifier = Modifier
             .fillMaxWidth()
     ) {
@@ -36,7 +45,7 @@ fun Conversation(
                 .padding(vertical = 10.dp, horizontal = 10.dp)
         ) {
             Image(
-                painter = rememberCoilPainter(request = data.user_image,
+                painter = rememberCoilPainter(request = cachedConversation.value.user_image,
                     requestBuilder = {
                         transformations(CircleCropTransformation())
                     }
@@ -51,33 +60,43 @@ fun Conversation(
                     .weight(2f)
                     .padding(start = 15.dp)
             ) {
-                Text(
-                    text = data.username,
-                    fontFamily = fontFamily,
-                    fontWeight = FontWeight.Medium,
-                    fontSize = 15.sp,
-                    modifier = Modifier.padding(bottom = 10.dp)
-                )
-                Text(
-                    text = data.last_message,
-                    fontSize = 13.sp,
-                    fontFamily = fontFamily,
-                    fontWeight = FontWeight.Normal,
-                    color = MaterialTheme.colors.onSecondary
-                )
-            }
-            Column(horizontalAlignment = Alignment.End, modifier = Modifier.padding(end = 10.dp)) {
-                Text(
-                    text = data.last_message_time,
-                    fontSize = 12.sp,
-                    fontFamily = fontFamily,
-                    fontWeight = FontWeight.Normal,
-                    modifier = Modifier.padding(bottom = 7.dp)
-                )
-                if (data.unread_messages > 0) Text(
-                    text = data.unread_messages.toString(),
-                    fontSize = 13.sp
-                )
+                Row(verticalAlignment = Alignment.CenterVertically) {
+                    Text(
+                        text = cachedConversation.value.username,
+                        fontFamily = fontFamily,
+                        fontWeight = FontWeight.Medium,
+                        fontSize = 15.sp,
+                        modifier = Modifier
+                            .weight(1f)
+                            .padding(bottom = 10.dp)
+                    )
+                    if (lastMessage != null) {
+                        Text(
+                            text = PrettyTime().format(lastMessage.created_at),
+                            fontSize = 12.sp,
+                            fontFamily = fontFamily,
+                            fontWeight = FontWeight.Normal,
+                            modifier = Modifier.padding(bottom = 7.dp)
+                        )
+                    }
+                }
+                Row(verticalAlignment = Alignment.CenterVertically) {
+                    Text(
+                        text = lastMessage?.content ?: "There is no message",
+                        fontSize = 13.sp,
+                        fontFamily = fontFamily,
+                        fontWeight = FontWeight.Normal,
+                        color = MaterialTheme.colors.onSecondary,
+                        lineHeight = 18.sp,
+                        maxLines = 1,
+                        modifier = Modifier.weight(1f)
+                    )
+                    Text(
+                        text = if (cachedConversation.value.unread_messages > 0) cachedConversation.value.unread_messages.toString() else "",
+                        fontSize = 13.sp
+                    )
+                }
+
             }
         }
     }
