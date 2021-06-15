84d8344412090d8755c43c623a6b7976bb52c896
TeamOF <shervin.ourfamily@gmail.com>
2021-06-15 13:06:01 +0430

    added update_at Date, and change created_at type to Date

--- app/src/main/java/net/teamof/whisper/models/Message.kt
+++ app/src/main/java/net/teamof/whisper/models/Message.kt
@@ -2,6 +2,7 @@ package net.teamof.whisper.models
 
 import io.objectbox.annotation.Entity
 import io.objectbox.annotation.Id
+import java.util.*
 
 @Entity
 data class Message(
@@ -10,5 +11,6 @@ data class Message(
     var channel: String = "",
     var user_id: Long = 0,
     var content: String = "",
-    var created_at: String
+    var created_at: Date = Date(),
+    var updated_at: Date? = null
 )
