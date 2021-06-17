ef5316510fae886c35e43a8786f06557573ae73d
TeamOF <shervin.ourfamily@gmail.com>
2021-06-17 13:53:15 +0430

    made @Serializable

--- app/src/main/java/net/teamof/whisper/models/Message.kt
+++ app/src/main/java/net/teamof/whisper/models/Message.kt
@@ -2,15 +2,20 @@ package net.teamof.whisper.models
 
 import io.objectbox.annotation.Entity
 import io.objectbox.annotation.Id
+import kotlinx.serialization.Serializable
+import net.teamof.whisper.utils.DateAsStringSerializer
 import java.util.*
 
 @Entity
+@Serializable
 data class Message(
     @Id
     var id: Long = 0,
     var channel: String = "",
     var user_id: Long = 0,
     var content: String = "",
+    @Serializable(with = DateAsStringSerializer::class)
     var created_at: Date = Date(),
+    @Serializable(with = DateAsStringSerializer::class)
     var updated_at: Date? = null
 )
