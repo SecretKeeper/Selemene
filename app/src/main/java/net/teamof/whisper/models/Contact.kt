49c079ea45eaa05cdb623ff35b117f291fdac0a3
TeamOF <shervin.ourfamily@gmail.com>
2021-06-17 13:53:05 +0430

    made @Serializable

--- app/src/main/java/net/teamof/whisper/models/Contact.kt
+++ app/src/main/java/net/teamof/whisper/models/Contact.kt
@@ -4,8 +4,10 @@ import io.objectbox.annotation.Entity
 import io.objectbox.annotation.Id
 import io.objectbox.annotation.Index
 import io.objectbox.annotation.Unique
+import kotlinx.serialization.Serializable
 
 @Entity
+@Serializable
 data class Contact(
     @Id
     var id: Long = 0,
