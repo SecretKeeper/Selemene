14b17685c0c89a48c22b8f285a4b53e3515a3ff3
TeamOF <shervin.ourfamily@gmail.com>
2021-06-19 16:35:22 +0430

    implement websocket here for now

--- app/src/main/java/net/teamof/whisper/screens/MainScreen.kt
+++ app/src/main/java/net/teamof/whisper/screens/MainScreen.kt
@@ -7,6 +7,7 @@ import androidx.compose.material.FabPosition
 import androidx.compose.material.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
@@ -16,6 +17,7 @@ import net.teamof.whisper.components.BottomAppBar
 import net.teamof.whisper.components.FloatingActionButton
 import net.teamof.whisper.ui.theme.WhisperTheme
 import net.teamof.whisper.utils.EchoWebSocketListener
+import net.teamof.whisper.viewModel.FeedsViewModel
 import okhttp3.OkHttpClient
 import okhttp3.Request
 
@@ -81,7 +83,10 @@ private fun MainScreenNavigationConfigurations(navController: NavHostController)
                 channel = backStackEntry.arguments?.getString("channel")!!
             )
         }
-        composable("Feeds") { Feeds() }
+        composable("Feeds") { backStackEntry ->
+            val feedsViewModel = hiltViewModel<FeedsViewModel>()
+            Feeds(feedsViewModel)
+        }
         composable("Create") { Create(navController) }
         composable("Contacts/{action}") { backStackEntry ->
             backStackEntry.arguments?.getString("action")?.let {
