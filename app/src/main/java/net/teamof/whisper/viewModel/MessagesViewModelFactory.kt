81de0465b6abda63b0a0da118ac1f934552fd23a
Secret Keeper <shervin.ourfamily@gmail.com>
2021-06-20 02:46:15 +0430

    removed channel from constructor

--- app/src/main/java/net/teamof/whisper/viewModel/MessagesViewModelFactory.kt
+++ app/src/main/java/net/teamof/whisper/viewModel/MessagesViewModelFactory.kt
@@ -3,7 +3,7 @@ package net.teamof.whisper.viewModel
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 
-class MessagesViewModelFactory(private val channel: String) :
+class MessagesViewModelFactory :
     ViewModelProvider.NewInstanceFactory() {
-    override fun <T : ViewModel?> create(modelClass: Class<T>): T = MessagesViewModel(channel) as T
+    override fun <T : ViewModel?> create(modelClass: Class<T>): T = MessagesViewModel() as T
 }
