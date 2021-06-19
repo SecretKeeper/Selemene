2ce4dd97a9785111c66a3dd0b32bf9924b807978
TeamOF <shervin.ourfamily@gmail.com>
2021-06-19 16:30:50 +0430

    init

---
+++ app/src/main/java/net/teamof/whisper/utils/WebSocketInitializer.kt
@@ -0,0 +1,34 @@
+package net.teamof.whisper.utils
+
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ActivityComponent
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import okhttp3.WebSocket
+
+@Module
+@InstallIn(ActivityComponent::class)
+object WebSocketInitializer {
+
+    private const val WebSocketPath = "ws://10.0.2.2:3334/ws"
+
+    private val HttpClient = OkHttpClient()
+
+    private val WebSocketListener = EchoWebSocketListener()
+
+    @Provides
+    fun WebSocket(): WebSocket {
+        val webSocket = HttpClient.newWebSocket(makeRequest(), WebSocketListener)
+
+        HttpClient.dispatcher().executorService().shutdown()
+
+        return webSocket
+    }
+
+    private fun makeRequest(): Request {
+        return Request.Builder().url(WebSocketPath).build()
+    }
+
+}
