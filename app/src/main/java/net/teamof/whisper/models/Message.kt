1644c43bf69996c24d25b99bcb42fa08f0e44fcf
Secret Keeper <shervin.ourfamily@gmail.com>
2021-08-29 14:28:38 +0430

    make required created_at field

--- app/src/main/java/net/teamof/whisper/models/Message.kt
+++ app/src/main/java/net/teamof/whisper/models/Message.kt
@@ -16,6 +16,6 @@ data class Message(
     @Json(name = "userId")
     var user_id: Long = 0,
     var content: String = "",
-    var created_at: Date? = Date(),
+    var created_at: Date = Date(),
     var updated_at: Date? = null,
 )
