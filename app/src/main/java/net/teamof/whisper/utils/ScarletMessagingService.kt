0614778c4aec70b5f51db85e1dd99a2020bc3e0c
Secret Keeper <shervin.ourfamily@gmail.com>
2021-09-01 13:50:41 +0430

    init

---
+++ app/src/main/java/net/teamof/whisper/utils/ScarletMessagingService.kt
@@ -0,0 +1,21 @@
+package net.teamof.whisper.utils
+
+import com.tinder.scarlet.WebSocket
+import com.tinder.scarlet.ws.Receive
+import com.tinder.scarlet.ws.Send
+import kotlinx.coroutines.flow.Flow
+
+import net.teamof.whisper.models.Message
+import net.teamof.whisper.models.WSSubscribeChannels
+
+
+interface ScarletMessagingService {
+    @Receive
+    fun observeWebSocket(): Flow<WebSocket.Event>
+
+    @Send
+    fun sendSubscribe(subscribe: WSSubscribeChannels)
+
+    @Receive
+    fun observeMessage(): Flow<Message>
+}
