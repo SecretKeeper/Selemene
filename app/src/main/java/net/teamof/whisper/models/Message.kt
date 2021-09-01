570067cf1fcc070b158b659316a3df0eab338e16
Secret Keeper <shervin.ourfamily@gmail.com>
2021-09-01 16:04:21 +0430

    change moshi userId column name to default user_id

--- app/src/main/java/net/teamof/whisper/models/Message.kt
+++ app/src/main/java/net/teamof/whisper/models/Message.kt
@@ -1,6 +1,5 @@
 package net.teamof.whisper.models
 
-import com.squareup.moshi.Json
 import com.squareup.moshi.JsonClass
 import io.objectbox.annotation.Entity
 import io.objectbox.annotation.Id
@@ -13,7 +12,6 @@ data class Message(
     var id: Long = 0,
     var type: String = "message",
     var channel: String = "",
-    @Json(name = "userId")
     var user_id: Long = 0,
     var content: String = "",
     var created_at: Date = Date(),
