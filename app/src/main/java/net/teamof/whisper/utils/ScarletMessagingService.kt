1213795f18e000243f8e9655f67b0b250d7a3298
Secret Keeper <shervin.ourfamily@gmail.com>
2021-09-01 16:05:41 +0430

    add @Send sendMessage

--- app/src/main/java/net/teamof/whisper/utils/ScarletMessagingService.kt
+++ app/src/main/java/net/teamof/whisper/utils/ScarletMessagingService.kt
@@ -8,13 +8,15 @@ import kotlinx.coroutines.flow.Flow
 import net.teamof.whisper.models.Message
 import net.teamof.whisper.models.WSSubscribeChannels
 
-
 interface ScarletMessagingService {
     @Receive
     fun observeWebSocket(): Flow<WebSocket.Event>
 
     @Send
     fun sendSubscribe(subscribe: WSSubscribeChannels)
+
+    @Send
+    fun sendMessage(message: Message)
 
     @Receive
     fun observeMessage(): Flow<Message>
