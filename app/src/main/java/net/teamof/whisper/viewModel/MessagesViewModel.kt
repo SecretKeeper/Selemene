c8e0059e5445759b97731fc142911a1bff91dfed
SecretsNetwork <shervin.ourfamily@gmail.com>
2021-09-06 16:51:52 +0430

    change channel implementation to to_user_id

--- app/src/main/java/net/teamof/whisper/viewModel/MessagesViewModel.kt
+++ app/src/main/java/net/teamof/whisper/viewModel/MessagesViewModel.kt
@@ -3,8 +3,6 @@ package net.teamof.whisper.viewModel
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.squareup.moshi.Moshi
-import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 import com.tinder.scarlet.WebSocket
 import dagger.hilt.android.lifecycle.HiltViewModel
 import io.objectbox.Box
@@ -16,7 +14,6 @@ import net.teamof.whisper.ObjectBox
 import net.teamof.whisper.models.Message
 import net.teamof.whisper.models.Message_
 import net.teamof.whisper.models.WSSubscribeChannels
-import net.teamof.whisper.utils.DateMoshiAdapter
 import net.teamof.whisper.utils.ScarletMessagingService
 import timber.log.Timber
 import java.util.*
@@ -55,12 +52,6 @@ class MessagesViewModel
 
     private val messageBox: Box<Message> = ObjectBox.store.boxFor(Message::class.java)
 
-    private val moshiMessageAdapter =
-        Moshi.Builder()
-            .add(Date::class.java, DateMoshiAdapter().nullSafe())
-            .addLast(KotlinJsonAdapterFactory())
-            .build().adapter(Message::class.java)
-
     private val _messages: MutableLiveData<MutableList<Message>> by lazy {
         MutableLiveData<MutableList<Message>>()
     }
@@ -82,18 +73,18 @@ class MessagesViewModel
         _messages.value = (_messages.value)?.let { mutableListOf(*it.toTypedArray(), message) }
     }
 
-    fun getLastMessage(channel: String): Message? {
+    fun getLastMessage(to_user_id: Long): Message? {
         val query =
-            messageBox.query().equal(Message_.channel, channel).orderDesc(Message_.id).build()
+            messageBox.query().equal(Message_.to_user_id, to_user_id).orderDesc(Message_.id).build()
         val result = query.findFirst()
         query.close()
 
         return result
     }
 
-    fun getMessagesByChannel(channel: String) {
+    fun getMessagesByChannel(to_user_id: Long) {
         val query =
-            messageBox.query().equal(Message_.channel, channel).order(Message_.created_at)
+            messageBox.query().equal(Message_.to_user_id, to_user_id).order(Message_.created_at)
                 .build()
         val result = query.find()
         query.close()
