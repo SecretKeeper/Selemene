17b1c0aac288b3c61e24abc547842f35f7845f60
SecretsNetwork <shervin.ourfamily@gmail.com>
2021-09-20 01:20:03 +0430

    implement authenticate and sendSubscribeChannels methods

--- app/src/main/java/net/teamof/whisper/viewModel/UserViewModel.kt
+++ app/src/main/java/net/teamof/whisper/viewModel/UserViewModel.kt
@@ -4,18 +4,51 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.asLiveData
 import dagger.hilt.android.lifecycle.HiltViewModel
+import io.objectbox.Box
+import net.teamof.whisper.ObjectBox
 import net.teamof.whisper.di.DataStoreManager
+import net.teamof.whisper.models.Conversation
+import net.teamof.whisper.models.WSSubscribeChannels
+import net.teamof.whisper.utils.ScarletMessagingService
 import javax.inject.Inject
 
 @HiltViewModel
-class UserViewModel @Inject constructor(private val dataStoreManager: DataStoreManager) :
+class UserViewModel @Inject constructor(
+    private val scarletMessagingService: ScarletMessagingService,
+    private val dataStoreManager: DataStoreManager
+) :
     ViewModel() {
+
+    private val conversationBox: Box<Conversation> =
+        ObjectBox.store.boxFor(Conversation::class.java)
+
     fun getUserID(): LiveData<Long> {
         return dataStoreManager.getUserId().asLiveData()
     }
 
     suspend fun setUserID(userID: Long) {
         dataStoreManager.setUserId(userID)
+    }
+
+    suspend fun authenticate(userID: Long) {
+        setUserID(userID)
+        sendSubscribeChannels(userID)
+    }
+
+    private fun sendSubscribeChannels(userID: Long) {
+
+        val channels = arrayListOf<String>()
+        val existsChannels = conversationBox.all
+
+        existsChannels.map { conversation -> channels.add(conversation.to_user_id.toString()) }
+
+        scarletMessagingService.sendSubscribe(
+            WSSubscribeChannels(
+                userID,
+                "subscribe-channels",
+                channels
+            )
+        )
     }
 
 }
