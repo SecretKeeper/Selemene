831990db3529974559746918f0d671ed200b9622
SecretsNetwork <shervin.ourfamily@gmail.com>
2021-09-22 10:30:23 +0330

    redesigned login form

--- app/src/main/java/net/teamof/whisper/screens/Login.kt
+++ app/src/main/java/net/teamof/whisper/screens/Login.kt
@@ -1,18 +1,25 @@
 package net.teamof.whisper.screens
 
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.material.TextButton
-import androidx.compose.material.TextField
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
-import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import kotlinx.coroutines.launch
+import net.teamof.whisper.components.TextField
+import net.teamof.whisper.ui.theme.fontFamily
 import net.teamof.whisper.viewModel.UserViewModel
 
 @Composable()
@@ -20,35 +27,98 @@ fun LoginScreen(
     userViewModel: UserViewModel,
     navController: NavController
 ) {
-
-    val currentUserId = userViewModel.getUserID().observeAsState()
-
     val composableScope = rememberCoroutineScope()
-    val setUserId =
-        remember { mutableStateOf(if (currentUserId.value != null) currentUserId.value else 0) }
+    val username = remember { mutableStateOf("") }
+    val password = remember { mutableStateOf("") }
 
-    Column {
-        Text(text = "It's Login Form")
-        Text(text = currentUserId.value.toString())
-        TextField(
-            value = setUserId.value.toString(),
-            onValueChange = { setUserId.value = it.toLong() },
-            keyboardOptions = KeyboardOptions(
-                keyboardType = KeyboardType.Number
-            ),
-        )
-        TextButton(onClick = {
-
-            composableScope.launch {
-                setUserId.value?.let { userViewModel.authenticate(it) }
+    Column(Modifier.padding(horizontal = 25.dp, vertical = 15.dp)) {
+        Column(modifier = Modifier.weight(1f)) {
+            Text(
+                text = "Welcome to Whispers!",
+                textAlign = TextAlign.Center,
+                fontFamily = fontFamily,
+                fontSize = 26.sp,
+                fontWeight = FontWeight(700),
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(bottom = 15.dp)
+            )
+            Text(
+                text = "Keep your data safe",
+                textAlign = TextAlign.Center,
+                color = Color(97, 102, 125),
+                fontFamily = fontFamily,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(bottom = 35.dp)
+            )
+            TextField(
+                text = "Username",
+                value = username.value,
+                setValue = { username.value = it }
+            )
+            TextField(
+                text = "Password",
+                value = password.value,
+                setValue = { password.value = it },
+                type = "password"
+            )
+            Button(
+                onClick = {
+                    composableScope.launch {
+                        username.value.let {
+                            userViewModel.authenticate(
+                                username.value,
+                                password.value
+                            )
+                        }
+                    }
+                    navController.navigate("Conversations") {
+                        launchSingleTop = true
+                        popUpTo("Login") { inclusive = true }
+                    }
+                }) {
+                Text(
+                    text = "Sign In",
+                    color = Color.White,
+                    textAlign = TextAlign.Center,
+                    fontFamily = fontFamily,
+                    modifier = Modifier
+                        .padding(vertical = 6.dp)
+                        .fillMaxWidth()
+                )
             }
-
-            navController.navigate("Conversations") {
-                launchSingleTop = true
-                popUpTo("Login") { inclusive = true }
+            TextButton(
+                onClick = {
+                    navController.navigate("Conversations") {
+                        launchSingleTop = true
+                        popUpTo("Login") { inclusive = true }
+                    }
+                }) {
+                Text(
+                    text = "Forgot Password?",
+                    textAlign = TextAlign.Center,
+                    modifier = Modifier
+                        .padding(vertical = 6.dp)
+                        .fillMaxWidth()
+                )
             }
-        }) {
-            Text(text = "Go to first destination")
+        }
+
+        TextButton(
+            onClick = {
+                navController.navigate("Conversations") {
+                    launchSingleTop = true
+                    popUpTo("Login") { inclusive = true }
+                }
+            }) {
+            Text(
+                text = "Does not have an account yet?",
+                textAlign = TextAlign.Center,
+                modifier = Modifier
+                    .padding(vertical = 6.dp)
+                    .fillMaxWidth()
+            )
         }
     }
 }
