de2c2cdb62c8fe4dde4ab9d00c77f1fbf0af05ff
Secret Keeper <shervin.ourfamily@gmail.com>
2021-10-02 14:18:36 +0330

    add retrofit injection

--- app/src/main/java/net/teamof/whisper/di/AppModule.kt
+++ app/src/main/java/net/teamof/whisper/di/AppModule.kt
@@ -12,15 +12,23 @@ import dagger.hilt.InstallIn
 import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
 import net.teamof.whisper.Whisper
+import net.teamof.whisper.api.AuthAPI
 import net.teamof.whisper.sockets.FlowStreamAdapter
 import net.teamof.whisper.utils.DateMoshiAdapter
 import net.teamof.whisper.utils.ScarletMessagingService
 import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
 import java.util.*
+import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 class AppModule {
+
+    private val baseWebSocketAddress = "ws://10.0.2.2:3334/ws"
+
+    private val baseGateWayAddress = "http://10.0.2.2:3334"
 
     @Provides
     fun providesApplication(@ApplicationContext context: Context): Whisper {
@@ -48,12 +56,24 @@ class AppModule {
         moshi: Moshi
     ): Scarlet {
         return Scarlet.Builder()
-            .webSocketFactory(client.newWebSocketFactory("ws://10.0.2.2:3334/ws"))
+            .webSocketFactory(client.newWebSocketFactory(baseWebSocketAddress))
             .addMessageAdapterFactory(MoshiMessageAdapter.Factory(moshi))
             .addStreamAdapterFactory(FlowStreamAdapter.Factory())
 //            .lifecycle(AndroidLifecycle.ofApplicationForeground(application)) // when add this it does not work!
             .build()
     }
+
+    @Singleton
+    @Provides
+    fun provideRetrofit(okHttpClient: OkHttpClient): Retrofit = Retrofit.Builder()
+        .addConverterFactory(MoshiConverterFactory.create())
+        .baseUrl(baseGateWayAddress)
+        .client(okHttpClient)
+        .build()
+
+    @Singleton
+    @Provides
+    fun provideAuthAPI(retrofit: Retrofit) = retrofit.create(AuthAPI::class.java)
 
     @Provides
     fun provideScarletMessagingService(scarlet: Scarlet): ScarletMessagingService {
