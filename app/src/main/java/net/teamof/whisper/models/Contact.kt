ffa439cee9fe72063e0f6eb0119bc053a5972e61
Secret Keeper <shervin.ourfamily@gmail.com>
2021-10-09 15:17:39 +0330

    set default status and generate for moshi

--- app/src/main/java/net/teamof/whisper/models/Contact.kt
+++ app/src/main/java/net/teamof/whisper/models/Contact.kt
@@ -1,16 +1,18 @@
 package net.teamof.whisper.models
 
+import com.squareup.moshi.JsonClass
 import io.objectbox.annotation.Entity
 import io.objectbox.annotation.Id
 import io.objectbox.annotation.Index
 import io.objectbox.annotation.Unique
 
 @Entity
+@JsonClass(generateAdapter = true)
 data class Contact(
     @Id
     var id: Long = 0,
     @Index @Unique val user_id: Long,
     val username: String,
     val user_image: String,
-    val status: String
+    val status: String = "None"
 )
