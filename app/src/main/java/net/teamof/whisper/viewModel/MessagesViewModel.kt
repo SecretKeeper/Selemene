52261da38a7cf799c04beb0ad2a70e32a1caa954
Secret Keeper <shervin.ourfamily@gmail.com>
2021-10-27 07:19:41 +0330

    send subscribe channel, adapt MessageRepository

--- app/src/main/java/net/teamof/whisper/viewModel/MessagesViewModel.kt
+++ app/src/main/java/net/teamof/whisper/viewModel/MessagesViewModel.kt
@@ -1,17 +1,19 @@
 package net.teamof.whisper.viewModel
 
 import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.tinder.scarlet.WebSocket
 import dagger.hilt.android.lifecycle.HiltViewModel
 import io.objectbox.Box
+import io.objectbox.kotlin.equal
+import io.objectbox.kotlin.or
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.flowOn
 import kotlinx.coroutines.flow.launchIn
 import kotlinx.coroutines.flow.onEach
 import net.teamof.whisper.ObjectBox
 import net.teamof.whisper.models.*
+import net.teamof.whisper.repositories.MessageRepository
 import net.teamof.whisper.utils.ScarletMessagingService
 import timber.log.Timber
 import javax.inject.Inject
@@ -19,27 +21,21 @@ import javax.inject.Inject
 @HiltViewModel
 class MessagesViewModel
 @Inject constructor(
-    private val scarletMessagingService: ScarletMessagingService
+    private val scarletMessagingService: ScarletMessagingService,
+    private val messageRepository: MessageRepository
 ) :
-    ViewModel() {
+    ConversationsViewModel() {
 
     init {
 
         scarletMessagingService.observeWebSocket().flowOn(Dispatchers.IO).onEach {
             when (it) {
                 is WebSocket.Event.OnConnectionOpened<*> -> sendSubscribeChannels()
-                is WebSocket.Event.OnConnectionClosing -> Timber.d(
-                    "Socket",
-                    "Connection closing"
-                )
-                is WebSocket.Event.OnConnectionClosed -> Timber.d("Socket", "Connection closed")
-                is WebSocket.Event.OnConnectionFailed -> Timber.e(
-                    "Socket",
-                    "Connection failed",
-                    it.throwable
-                )
-                is WebSocket.Event.OnMessageReceived -> Timber.d("Socket", "Message received")
-                else -> Timber.d("Socket", it.toString())
+                is WebSocket.Event.OnConnectionClosing -> Timber.d("Socket Connection closing")
+                is WebSocket.Event.OnConnectionClosed -> Timber.d("Socket Connection closed")
+                is WebSocket.Event.OnConnectionFailed -> Timber.e(it.throwable)
+                is WebSocket.Event.OnMessageReceived -> Timber.d("Socket Message Received ${it.message}")
+                else -> Timber.d("Socket $it")
             }
         }.launchIn(viewModelScope)
 
@@ -56,6 +52,11 @@ class MessagesViewModel
 
     private val messageBox: Box<Message> = ObjectBox.store.boxFor(Message::class.java)
 
+    private val currentUserId = OBKeyValueBox.query().run {
+        equal(OBKeyValue_.key, "user_id")
+        build()
+    }.use { it.findFirst() }
+
     private val _messages: MutableLiveData<MutableList<Message>> by lazy {
         MutableLiveData<MutableList<Message>>()
     }
@@ -66,16 +67,12 @@ class MessagesViewModel
 
         val channels = arrayListOf<String>()
 
-        val query = OBKeyValueBox.query().equal(OBKeyValue_.key, "user_id").build()
-        val result = query.findFirst()
-        query.close()
-
-        if (result != null) {
-            channels.add(result.value)
+        if (currentUserId != null) {
+            channels.add(currentUserId.value)
 
             scarletMessagingService.sendSubscribe(
                 WSSubscribeChannels(
-                    result.value,
+                    currentUserId.value,
                     "subscribe-channels",
                     channels
                 )
@@ -85,34 +82,45 @@ class MessagesViewModel
 
     fun sendMessage(message: Message) {
 
-        saveMessage(message)
+        messageRepository.saveMessage(message)
+
+        _messages.value = (_messages.value)?.let { mutableListOf(*it.toTypedArray(), message) }
 
         scarletMessagingService.sendMessage(message)
     }
 
     private fun saveMessage(message: Message) {
-        messageBox.put((message))
+        messageRepository.saveMessage((message))
+
+        refreshConversations()
 
         _messages.value = (_messages.value)?.let { mutableListOf(*it.toTypedArray(), message) }
     }
 
     fun getLastMessage(to_user_id: Long): Message? {
-        val query =
-            messageBox.query().equal(Message_.to_user_id, to_user_id).orderDesc(Message_.id).build()
-        val result = query.findFirst()
-        query.close()
+        val lastMessage =
+            messageBox.query().run {
+                equal(Message_.to_user_id, to_user_id)
+                orderDesc(Message_.id)
+                build()
+            }.use { it.findFirst() }
 
-        return result
+        return lastMessage
     }
 
-    fun getMessagesByChannel(to_user_id: Long) {
-        val query =
-            messageBox.query().equal(Message_.to_user_id, to_user_id).order(Message_.created_at)
-                .build()
-        val result = query.find()
-        query.close()
+    fun getConversationMessages(to_user_id: Long) {
+        if (currentUserId != null) {
 
-        _messages.value = result
+            val messages =
+                messageBox.query().run {
+                    (Message_.to_user_id equal to_user_id
+                            or (Message_.user_id equal currentUserId.value))
+                    order(Message_.created_at)
+                    build()
+                }.use { it.find() }
+
+            _messages.value = messages
+        }
     }
 
 }
