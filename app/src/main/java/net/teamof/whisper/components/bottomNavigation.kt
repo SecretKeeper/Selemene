dce87f2c3e0df67343b9df7899331a69e442bcce
Secret Keeper <shervin.ourfamily@gmail.com>
2021-10-29 16:36:24 +0330

    removed FAB and implement conversation actions

--- app/src/main/java/net/teamof/whisper/components/bottomNavigation.kt
+++ app/src/main/java/net/teamof/whisper/components/bottomNavigation.kt
@@ -1,19 +1,21 @@
 package net.teamof.whisper.components
 
+import androidx.compose.animation.*
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.vectorResource
-import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import net.teamof.whisper.R
 import net.teamof.whisper.ui.theme.BottomNavigationHeight
-import net.teamof.whisper.ui.theme.BottomNavigationIconSize
+import net.teamof.whisper.viewModel.ConversationActionsViewModel
 
 sealed class NavItem(
     val route: String,
@@ -32,9 +34,9 @@ sealed class NavItem(
         true
     )
 
-    object Holder : NavItem(
-        "holder",
-        R.drawable.ic_settings,
+    object Create : NavItem(
+        "create",
+        R.drawable.ic_add_nav,
         false
     )
 
@@ -51,14 +53,18 @@ sealed class NavItem(
     )
 }
 
+@ExperimentalAnimationApi
 @Composable
-fun BottomAppBar(navController: NavController) {
+fun BottomAppBar(
+    navController: NavController,
+    conversationActionsViewModel: ConversationActionsViewModel
+) {
 
 
     val items = listOf(
         NavItem.Messages,
         NavItem.Feeds,
-        NavItem.Holder,
+        NavItem.Create,
         NavItem.Activities,
         NavItem.Settings
     )
@@ -69,55 +75,114 @@ fun BottomAppBar(navController: NavController) {
         contentPadding = PaddingValues(horizontal = 0.dp, vertical = 0.dp),
         modifier = Modifier.height(BottomNavigationHeight)
     ) {
-        BottomNavigation(
-            backgroundColor = Color(red = 245, green = 245, blue = 253),
-            elevation = 0.dp,
-            modifier = Modifier.height(BottomNavigationHeight)
-        ) {
 
-            val currentRoute = navController.currentBackStackEntry?.destination?.route
+        val showConversationActions: Boolean by conversationActionsViewModel.showActionsState.observeAsState(
+            false
+        )
 
-            items.forEach { screen ->
-                BottomNavigationItem(
-                    selected = currentRoute == screen.route,
-                    onClick = {
-                        navController.navigate(screen.route) {
-                            popUpTo = navController.graph.startDestinationId
-                            launchSingleTop = true
-                        }
-                    },
-                    icon = {
-                        Icon(
-                            painter = painterResource(id = screen.icon),
-                            contentDescription = null,
-                            modifier = Modifier.size(24.dp, 24.dp)
-                        )
-                    },
-                    enabled = screen.enabled,
+        AnimatedContent(
+            targetState = showConversationActions,
+            transitionSpec = {
+                if (showConversationActions) {
+                    slideInVertically({ height -> -height }) + fadeIn() with
+                            slideOutVertically({ height -> height }) + fadeOut()
+                } else {
+                    slideInVertically({ height -> height }) + fadeIn() with
+                            slideOutVertically({ height -> -height }) + fadeOut()
+                }.using(
+                    SizeTransform(clip = false)
                 )
             }
+        ) { showActions ->
+            if (showActions)
+                ConversationActionsView(conversationActionsViewModel)
+            else
+                BottomNavigationView(navController, items)
         }
     }
 }
 
 @Composable
-fun FloatingActionButton(navController: NavController) {
-    FloatingActionButton(
-        onClick = {
-            navController.navigate("create")
-        },
-        backgroundColor = MaterialTheme.colors.primary,
-        modifier = Modifier
-            .offset(Dp(0F), Dp(30F))
-            .size(40.dp),
+fun BottomNavigationView(navController: NavController, items: List<NavItem>) {
+    BottomNavigation(
+        backgroundColor = Color(red = 245, green = 245, blue = 253),
+        elevation = 0.dp,
+        modifier = Modifier.height(BottomNavigationHeight)
     ) {
-        Icon(
-            imageVector = ImageVector.vectorResource(id = R.drawable.ic_add),
-            contentDescription = null,
-            tint = Color.White,
-            modifier = Modifier
-                .width(BottomNavigationIconSize)
-                .height(BottomNavigationIconSize)
-        )
+
+        val currentRoute = navController.currentBackStackEntry?.destination?.route
+
+        items.forEach { screen ->
+            BottomNavigationItem(
+                selected = currentRoute == screen.route,
+                onClick = {
+                    navController.navigate(screen.route) {
+                        popUpTo = navController.graph.startDestinationId
+                        launchSingleTop = true
+                    }
+                },
+                icon = {
+                    if (screen.route != "create") Icon(
+                        imageVector = ImageVector.vectorResource(id = screen.icon),
+                        contentDescription = null,
+                        modifier = Modifier.size(24.dp, 24.dp)
+                    )
+                    else
+                        IconButton(
+                            onClick = {
+                                navController.navigate(screen.route) {
+                                    popUpTo = navController.graph.startDestinationId
+                                    launchSingleTop = true
+                                }
+                            }) {
+                            Icon(
+                                imageVector = ImageVector.vectorResource(id = screen.icon),
+                                tint = Color.Unspecified,
+                                contentDescription = null,
+                                modifier = Modifier.size(48.dp)
+                            )
+                        }
+                },
+                enabled = screen.enabled
+            )
+        }
+    }
+}
+
+@Composable
+fun ConversationActionsView(conversationActionsViewModel: ConversationActionsViewModel) {
+    Row(
+        verticalAlignment = Alignment.CenterVertically,
+        modifier = Modifier.padding(horizontal = 8.dp)
+    ) {
+        Row(verticalAlignment = Alignment.CenterVertically, modifier = Modifier.weight(1f)) {
+            IconButton(onClick = { /*TODO*/ }) {
+                Icon(
+                    imageVector = ImageVector.vectorResource(id = R.drawable.ic_mute),
+                    contentDescription = null,
+                    modifier = Modifier
+                        .width(24.dp)
+                )
+            }
+            IconButton(onClick = { /*TODO*/ }) {
+                Icon(
+                    imageVector = ImageVector.vectorResource(id = R.drawable.ic_trash),
+                    contentDescription = null,
+                    modifier = Modifier
+                        .width(22.dp)
+                )
+            }
+        }
+        IconButton(onClick = {
+            conversationActionsViewModel.hideActions()
+        }) {
+            Icon(
+                imageVector = ImageVector.vectorResource(id = R.drawable.ic_x),
+                contentDescription = null,
+                modifier = Modifier
+                    .width(25.dp)
+                    .height(25.dp)
+            )
+        }
     }
 }
