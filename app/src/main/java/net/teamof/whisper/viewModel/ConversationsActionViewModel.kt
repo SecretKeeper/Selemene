af9d0c507e75163e07c4b88b85eb4f2dd24ae8d9
Secret Keeper <shervin.ourfamily@gmail.com>
2021-10-29 16:35:30 +0330

    init

---
+++ app/src/main/java/net/teamof/whisper/viewModel/ConversationsActionViewModel.kt
@@ -0,0 +1,49 @@
+package net.teamof.whisper.viewModel
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class ConversationActionsViewModel : ViewModel() {
+
+    private val _showActionsState: MutableLiveData<Boolean> by lazy {
+        MutableLiveData<Boolean>(false)
+    }
+
+    val showActionsState: MutableLiveData<Boolean> = _showActionsState
+
+    private val _selectedConversations: MutableLiveData<List<Long>> by lazy {
+        MutableLiveData<List<Long>>(mutableListOf())
+    }
+
+    val selectedConversations: MutableLiveData<List<Long>> = _selectedConversations
+
+
+    fun showActions(conversation_id: Long) {
+        _selectedConversations.value = (_selectedConversations.value)?.let {
+            mutableListOf(
+                *it.toTypedArray(),
+                conversation_id
+            )
+        }
+        _showActionsState.value = true
+    }
+
+    fun hideActions() {
+        _selectedConversations.value = mutableListOf()
+        _showActionsState.value = false
+    }
+
+    fun selectConversation(conversation_id: Long) {
+        _selectedConversations.value = (_selectedConversations.value)?.let {
+            mutableListOf(
+                *it.toTypedArray(),
+                conversation_id
+            )
+        }
+    }
+
+    fun unselectConversation(conversation_id: Long) {
+        _selectedConversations.value =
+            _selectedConversations.value?.filter { it != conversation_id }
+    }
+}
