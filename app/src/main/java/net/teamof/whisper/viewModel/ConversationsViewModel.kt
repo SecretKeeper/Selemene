14846c16b1be319ad79832ce7dbd14e95ed92824
Secret Keeper <shervin.ourfamily@gmail.com>
2021-10-30 06:21:02 +0330

    work with ObjectBoxLive data instead of MutableLiveData , also impelement delete single and multiple conversation

--- app/src/main/java/net/teamof/whisper/viewModel/ConversationsViewModel.kt
+++ app/src/main/java/net/teamof/whisper/viewModel/ConversationsViewModel.kt
@@ -1,9 +1,9 @@
 package net.teamof.whisper.viewModel
 
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
 import io.objectbox.Box
+import io.objectbox.android.ObjectBoxLiveData
 import net.teamof.whisper.ObjectBox
 import net.teamof.whisper.models.Conversation
 import net.teamof.whisper.models.Conversation_
@@ -15,14 +15,13 @@ open class ConversationsViewModel @Inject constructor() : ViewModel() {
     private val conversationBox: Box<Conversation> =
         ObjectBox.store.boxFor(Conversation::class.java)
 
-    private val _conversations: MutableLiveData<MutableList<Conversation>> by lazy {
-        MutableLiveData<MutableList<Conversation>>(conversationBox.all)
-    }
+    private var _conversations: ObjectBoxLiveData<Conversation> =
+        ObjectBoxLiveData(conversationBox.query().build())
 
-    val conversations: MutableLiveData<MutableList<Conversation>> = _conversations
+    val conversations: ObjectBoxLiveData<Conversation> = _conversations
 
     fun refreshConversations() {
-        _conversations.value = conversationBox.all
+        _conversations = ObjectBoxLiveData(conversationBox.query().build())
     }
 
     private fun isConversationExist(conversation: Conversation): Long {
@@ -34,22 +33,19 @@ open class ConversationsViewModel @Inject constructor() : ViewModel() {
         return result
     }
 
-    fun getConversation(to_user_id: Long): Conversation? {
-        val query = conversationBox.query().equal(Conversation_.to_user_id, to_user_id).build()
-        val result = query.findFirst()
-        query.close()
+    fun getConversation(to_user_id: Long): Conversation? =
+        conversationBox.query().run {
+            equal(Conversation_.to_user_id, to_user_id)
+            build()
+        }.use { it.findFirst() }
 
-        return result
-    }
 
     fun createConversation(conversation: Conversation) {
-
         if (isConversationExist(conversation) == 0L) {
             saveConversation(conversation) {
                 refreshConversations()
             }
         }
-
     }
 
     private fun saveConversation(conversation: Conversation, refresh: () -> Unit) {
@@ -57,12 +53,24 @@ open class ConversationsViewModel @Inject constructor() : ViewModel() {
         refresh()
     }
 
-    fun deleteConversation() {
-        conversationBox.removeAll()
-        refreshConversations()
+    fun deleteConversation(conversation: Long) {
+        conversationBox.query().run {
+            equal(Conversation_.id, conversation)
+            build()
+        }.use {
+            it.remove()
+            refreshConversations()
+        }
     }
 
-    fun deleteConversation(conversation: Array<Conversation>) {
+    fun deleteConversation(conversations: List<Long>) =
 
-    }
+        conversationBox.query().run {
+            `in`(Conversation_.id, conversations.toLongArray())
+            build()
+        }.use {
+            it.remove()
+            refreshConversations()
+        }
+
 }
