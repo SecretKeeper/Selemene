002e4eda24341317f6512d3bd94ae7376492ff24
Secret Keeper <shervin.ourfamily@gmail.com>
2021-11-01 12:21:31 +0330

    update conversations on message receive

--- app/src/main/java/net/teamof/whisper/viewModel/ConversationsViewModel.kt
+++ app/src/main/java/net/teamof/whisper/viewModel/ConversationsViewModel.kt
@@ -4,9 +4,12 @@ import androidx.lifecycle.ViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
 import io.objectbox.Box
 import io.objectbox.android.ObjectBoxLiveData
+import io.objectbox.kotlin.equal
 import net.teamof.whisper.ObjectBox
 import net.teamof.whisper.models.Conversation
 import net.teamof.whisper.models.Conversation_
+import net.teamof.whisper.models.Message
+import timber.log.Timber
 import javax.inject.Inject
 
 @HiltViewModel
@@ -24,14 +27,11 @@ open class ConversationsViewModel @Inject constructor() : ViewModel() {
         _conversations = ObjectBoxLiveData(conversationBox.query().build())
     }
 
-    private fun isConversationExist(conversation: Conversation): Long {
-        val query =
-            conversationBox.query().equal(Conversation_.to_user_id, conversation.to_user_id).build()
-        val result = query.count()
-        query.close();
-
-        return result
-    }
+    private fun isConversationExist(to_user_id: Long): Long =
+        conversationBox.query().run {
+            (Conversation_.to_user_id equal to_user_id)
+            build()
+        }.use { it.count() }
 
     fun getConversation(to_user_id: Long): Conversation? =
         conversationBox.query().run {
@@ -39,9 +39,8 @@ open class ConversationsViewModel @Inject constructor() : ViewModel() {
             build()
         }.use { it.findFirst() }
 
-
     fun createConversation(conversation: Conversation) {
-        if (isConversationExist(conversation) == 0L) {
+        if (isConversationExist(conversation.to_user_id) == 0L) {
             saveConversation(conversation) {
                 refreshConversations()
             }
@@ -51,6 +50,35 @@ open class ConversationsViewModel @Inject constructor() : ViewModel() {
     private fun saveConversation(conversation: Conversation, refresh: () -> Unit) {
         conversationBox.put(conversation)
         refresh()
+    }
+
+    fun updateConversation(to_user_id: Long, newMessage: Message) {
+
+        if (isConversationExist(to_user_id) == 0L) {
+            Timber.d("We Created a new conversation Sorry! ${isConversationExist(to_user_id)}")
+            createConversation(
+                Conversation(
+                    to_user_id = newMessage.to_user_id,
+                    last_message = newMessage.content,
+                    last_message_time = newMessage.created_at,
+                    unread_messages = 0,
+                    username = "The Trick is Working",
+                    user_image = "https://cdn-icons-png.flaticon.com/128/924/924874.png"
+                )
+            )
+        } else {
+            conversationBox.query().run {
+                (Conversation_.to_user_id equal to_user_id)
+                build()
+            }.use {
+                val result = it.findFirst()
+                if (result != null) {
+                    result.last_message = newMessage.content
+                    result.last_message_time = newMessage.created_at
+                    conversationBox.put(result)
+                }
+            }
+        }
     }
 
     fun deleteConversation(conversation: Long) {
@@ -72,5 +100,4 @@ open class ConversationsViewModel @Inject constructor() : ViewModel() {
             it.remove()
             refreshConversations()
         }
-
 }
