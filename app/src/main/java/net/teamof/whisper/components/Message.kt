5a17fe6ed19dd8d67c04cb131c71a29123dea35a
Secret Keeper <shervin.ourfamily@gmail.com>
2021-11-05 08:55:15 +0330

    add selection icon and optimize animation speed

--- app/src/main/java/net/teamof/whisper/components/Message.kt
+++ app/src/main/java/net/teamof/whisper/components/Message.kt
@@ -1,10 +1,8 @@
 package net.teamof.whisper.components
 
 
-import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.animation.ExperimentalAnimationApi
-import androidx.compose.animation.fadeIn
-import androidx.compose.animation.fadeOut
+import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.foundation.background
 import androidx.compose.foundation.gestures.detectTapGestures
 import androidx.compose.foundation.layout.*
@@ -15,9 +13,9 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.scale
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.input.pointer.pointerInput
@@ -42,85 +40,62 @@ fun Message(
     val currentUserId = userViewModel.getUserID()
     val isOwnMessage = data.user_id == currentUserId
     val messageSelected = remember { mutableStateOf(false) }
+    val selectIconScaleState =
+        animateFloatAsState(if (selection && messageSelected.value) 1f else if (selection && !messageSelected.value) 0.25f else 0.0f)
 
-    Row(
-        verticalAlignment = Alignment.CenterVertically,
+    Box(
         modifier = Modifier
             .padding(start = 10.dp, end = 15.dp)
             .fillMaxWidth()
             .pointerInput(Unit) {
                 detectTapGestures(
                     onLongPress = { enableSelectionMode() },
-                    onTap = { messageSelected.value = !messageSelected.value }
+                    onTap = {
+                        messageSelected.value = !messageSelected.value
+                    }
                 )
             }
     ) {
-        AnimatedVisibility(visible = selection) {
-            Column(
-                modifier = Modifier
-                    .width(23.dp)
-                    .height(23.dp)
-            ) {
-                AnimatedVisibility(
-                    visible = messageSelected.value,
-                    enter = fadeIn(),
-                    exit = fadeOut()
-                ) {
-                    Icon(
-                        imageVector = ImageVector.vectorResource(
-                            id = R.drawable.ic_checkmark
-                        ),
-                        tint = MaterialTheme.colors.primary,
-                        contentDescription = null,
-                        modifier = Modifier
-                            .width(23.dp)
-                            .height(23.dp)
-                    )
-                }
-                AnimatedVisibility(
-                    visible = !messageSelected.value,
-                    enter = fadeIn(),
-                    exit = fadeOut()
-                ) {
-                    Icon(
-                        imageVector = ImageVector.vectorResource(
-                            id = R.drawable.ic_circle
-                        ),
-                        tint = Color.DarkGray,
-                        contentDescription = null,
-                        modifier = Modifier
-                            .width(23.dp)
-                            .height(23.dp)
-                    )
-                }
-            }
-        }
+        Icon(
+            imageVector = ImageVector.vectorResource(id = R.drawable.ic_checkmark_conversation),
+            tint = Color.Unspecified,
+            contentDescription = null,
+            modifier = Modifier
+                .width(22.dp)
+                .scale(selectIconScaleState.value)
+        )
         Row(
             horizontalArrangement = if (isOwnMessage) Arrangement.End else Arrangement.Start,
             modifier = Modifier
                 .padding(start = 15.dp)
                 .fillMaxWidth()
         ) {
-            Text(
-                text = data.content,
-                color = if (isOwnMessage) Color.White else Color.Black,
-                fontSize = 14.sp,
-                lineHeight = 22.sp,
-                fontFamily = fontFamily,
-                fontWeight = FontWeight.Normal,
-                modifier = Modifier
-                    .padding(vertical = 15.dp)
-                    .clip(
-                        shape = RoundedCornerShape(
-                            topStart = 25.dp,
-                            topEnd = 25.dp,
-                            bottomStart = if (isOwnMessage) 25.dp else 2.dp,
-                            bottomEnd = if (isOwnMessage) 2.dp else 25.dp
+            Box {
+                Text(
+                    text = data.content,
+                    color = if (isOwnMessage) Color.White else Color.Black,
+                    fontSize = 14.sp,
+                    lineHeight = 22.sp,
+                    fontFamily = fontFamily,
+                    fontWeight = FontWeight.Normal,
+                    modifier = Modifier
+                        .padding(vertical = 15.dp)
+                        .clip(
+                            shape = RoundedCornerShape(
+                                topStart = 25.dp,
+                                topEnd = 25.dp,
+                                bottomStart = if (isOwnMessage) 25.dp else 2.dp,
+                                bottomEnd = if (isOwnMessage) 2.dp else 25.dp
+                            )
                         )
-                    )
-                    .background(if (isOwnMessage) MaterialTheme.colors.primary else Color(0xfff7f8f7))
-                    .padding(vertical = 10.dp, horizontal = 15.dp)
-            )
+                        .background(
+                            if (isOwnMessage) MaterialTheme.colors.primary else Color(
+                                0xfff7f8f7
+                            )
+                        )
+                        .padding(vertical = 10.dp, horizontal = 15.dp)
+                )
+            }
         }
     }
 }
