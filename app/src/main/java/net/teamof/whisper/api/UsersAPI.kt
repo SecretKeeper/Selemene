9c550d35b24fbc874b2b8f2a34d1b1064dc4d913
Secret Keeper <shervin.ourfamily@gmail.com>
2021-11-30 14:53:31 +0330

    code refactoring

--- app/src/main/java/net/teamof/whisper/api/UsersAPI.kt
+++ app/src/main/java/net/teamof/whisper/api/UsersAPI.kt
@@ -1,5 +1,6 @@
 package net.teamof.whisper.api
 
+import net.teamof.whisper.models.UserAPI
 import retrofit2.Call
 import retrofit2.http.GET
 import retrofit2.http.Headers
@@ -8,5 +9,5 @@ import retrofit2.http.Path
 interface UsersAPI {
     @GET("user/id/{id}")
     @Headers("Content-Type: application/json")
-    fun getUserProfile(@Path("id") user_id: Long): Call<User>
+    fun getUserProfile(@Path("id") user_id: Long): Call<UserAPI>
 }
