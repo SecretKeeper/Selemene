944357aa11045d66b9f28dcf02973cea850adacf
Secret Keeper <shervin.ourfamily@gmail.com>
2021-12-05 01:42:52 +0330

    create global state for showing UnderConstruction

--- app/src/main/java/net/teamof/whisper/ui/theme/Constants.kt
+++ app/src/main/java/net/teamof/whisper/ui/theme/Constants.kt
@@ -2,5 +2,6 @@ package net.teamof.whisper.ui.theme
 
 import androidx.compose.ui.unit.dp
 
+val showUnderConstruction = true
 val BottomNavigationHeight = 60.dp
 val BottomNavigationIconSize = 22.dp
