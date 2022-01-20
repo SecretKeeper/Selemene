e5a70ba02768e26f98d27669bc42e2b84b44207b
Secret Keeper <shervin.ourfamily@gmail.com>
2022-01-20 14:09:13 +0330

    make Serializable, and assignable id

--- app/src/main/java/net/teamof/whisper/models/Message.kt
+++ app/src/main/java/net/teamof/whisper/models/Message.kt
@@ -3,16 +3,17 @@ package net.teamof.whisper.models
 import com.squareup.moshi.JsonClass
 import io.objectbox.annotation.Entity
 import io.objectbox.annotation.Id
+import java.io.Serializable
 import java.util.*
 
 @Entity
 @JsonClass(generateAdapter = true)
 data class Message(
-    @Id
+    @Id(assignable = true)
     var id: Long = 0,
     var user_id: Long = 0,
     var to_user_id: Long = 0,
     var content: String = "",
     var created_at: Date = Date(),
     var updated_at: Date? = null,
-)
+) : Serializable
