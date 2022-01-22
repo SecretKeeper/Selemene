2331ff2749805966e6f24e5be543300b8641e651
Secret Keeper <shervin.ourfamily@gmail.com>
2022-01-22 13:34:02 +0330

    added new screens to nav host

--- app/src/main/java/net/teamof/whisper/screens/SelfProfile.kt
+++ app/src/main/java/net/teamof/whisper/screens/SelfProfile.kt
@@ -2,27 +2,23 @@ package net.teamof.whisper.screens
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.gestures.detectTapGestures
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.Icon
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.rotate
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.input.pointer.pointerInput
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.navigation.NavController
 import coil.compose.rememberImagePainter
 import coil.transform.CircleCropTransformation
-import net.teamof.whisper.R
+import net.teamof.whisper.components.settings.SettingsItem
 import net.teamof.whisper.ui.theme.fontFamily
 
 @Composable
-fun SelfProfile() {
+fun SelfProfile(navController: NavController) {
     Column {
 
 
@@ -61,7 +57,7 @@ fun SelfProfile() {
             }
             SettingsItem(title = "Notifications")
             SettingsItem(title = "Privacy")
-            SettingsItem(title = "Security")
+            SettingsItem(title = "Security") { navController.navigate("Security") }
             SettingsItem(title = "Devices")
         }
 
@@ -97,38 +93,5 @@ fun SelfProfile() {
         }
 
 
-    }
-}
-
-
-@Composable
-fun SettingsItem(title: String) {
-    Row(
-        verticalAlignment = Alignment.CenterVertically,
-        modifier = Modifier
-            .fillMaxWidth()
-            .padding(bottom = 25.dp)
-            .pointerInput(Unit) {
-                detectTapGestures(
-                    onTap = {
-
-                    }
-                )
-            }) {
-        Text(
-            text = title,
-            fontFamily = fontFamily,
-            fontWeight = FontWeight.Normal,
-            fontSize = 17.sp,
-            modifier = Modifier.weight(1f)
-        )
-        Icon(
-            painter = painterResource(id = R.drawable.ic_chevron_left),
-            contentDescription = null,
-            Modifier
-                .width(20.dp)
-                .height(20.dp)
-                .rotate(180f)
-        )
     }
 }
