d7f06689c9c4a77b19b139034b8d5938c511fe7f
Secret Keeper <shervin.ourfamily@gmail.com>
2022-01-23 13:51:51 +0330

    implement subtitle and titleColor

--- app/src/main/java/net/teamof/whisper/components/settings/SettingsItem.kt
+++ app/src/main/java/net/teamof/whisper/components/settings/SettingsItem.kt
@@ -8,6 +8,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.rotate
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
@@ -17,12 +18,17 @@ import net.teamof.whisper.R
 import net.teamof.whisper.ui.theme.fontFamily
 
 @Composable
-fun SettingsItem(title: String, event: (() -> Unit)? = null) {
+fun SettingsItem(
+    title: String,
+    subTitle: String? = null,
+    event: (() -> Unit)? = null,
+    titleColor: Color? = Color.Black
+) {
     Row(
         verticalAlignment = Alignment.CenterVertically,
         modifier = Modifier
             .fillMaxWidth()
-            .padding(bottom = 25.dp)
+            .padding(bottom = 30.dp)
             .pointerInput(Unit) {
                 detectTapGestures(
                     onTap = {
@@ -37,15 +43,30 @@ fun SettingsItem(title: String, event: (() -> Unit)? = null) {
             fontFamily = fontFamily,
             fontWeight = FontWeight.Normal,
             fontSize = 17.sp,
+            color = titleColor ?: Color.Black,
             modifier = Modifier.weight(1f)
         )
-        Icon(
-            painter = painterResource(id = R.drawable.ic_chevron_left),
-            contentDescription = null,
-            Modifier
-                .width(20.dp)
-                .height(20.dp)
-                .rotate(180f)
-        )
+        Row(verticalAlignment = Alignment.CenterVertically) {
+            if (subTitle != null) {
+                Text(
+                    text = subTitle,
+                    fontFamily = fontFamily,
+                    fontWeight = FontWeight.Normal,
+                    fontSize = 16.sp,
+                    color = Color.Gray,
+                    modifier = Modifier.padding(end = 10.dp)
+                )
+            }
+            Icon(
+                painter = painterResource(id = R.drawable.ic_chevron_left),
+                contentDescription = null,
+                tint = Color.Gray,
+                modifier = Modifier
+                    .width(18.dp)
+                    .height(18.dp)
+                    .rotate(180f)
+            )
+        }
+
     }
 }
