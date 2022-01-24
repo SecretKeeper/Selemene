8ee61675c6e587222165f2f54751bc6afb6c78e9
Secret Keeper <shervin.ourfamily@gmail.com>
2022-01-24 03:30:40 +0330

    init

---
+++ app/src/main/java/net/teamof/whisper/api/AccountAPI.kt
@@ -0,0 +1,56 @@
+package net.teamof.whisper.api
+
+import com.squareup.moshi.JsonClass
+import okhttp3.ResponseBody
+import retrofit2.Response
+import retrofit2.http.Body
+import retrofit2.http.Headers
+import retrofit2.http.POST
+
+@JsonClass(generateAdapter = true)
+data class ChangeUsernameResponse(
+    val message: String
+)
+
+@JsonClass(generateAdapter = true)
+data class ChangeUsernameRequest(
+    val newUsername: String,
+)
+
+
+@JsonClass(generateAdapter = true)
+data class ChangeEmailResponse(
+    val message: String
+)
+
+@JsonClass(generateAdapter = true)
+data class ChangeEmailRequest(
+    val newEmail: String,
+)
+
+
+@JsonClass(generateAdapter = true)
+data class ChangePasswordResponse(
+    val message: String
+)
+
+@JsonClass(generateAdapter = true)
+data class ChangePasswordRequest(
+    val current_password: String,
+    val new_password: String
+)
+
+
+interface AccountAPI {
+    @POST("user/change-username")
+    @Headers("Content-Type: application/json")
+    suspend fun changeUsername(@Body new_password: ChangeUsernameRequest): Response<ResponseBody>
+
+    @POST("user/change-email")
+    @Headers("Content-Type: application/json")
+    suspend fun changeEmail(@Body changeEmailRequest: ChangeEmailRequest): Response<ResponseBody>
+
+    @POST("user/change-password")
+    @Headers("Content-Type: application/json")
+    suspend fun changePassword(@Body changePasswordRequest: ChangePasswordRequest): Response<ResponseBody>
+}
