59e6dde23e989a9bdb0620f60675da704cc25c8f
Secret Keeper <shervin.ourfamily@gmail.com>
2022-01-26 04:22:45 +0330

    removed initializing datastore

--- app/src/main/java/net/teamof/whisper/MainActivity.kt
+++ app/src/main/java/net/teamof/whisper/MainActivity.kt
@@ -2,7 +2,6 @@ package net.teamof.whisper
 
 import LocalBackPressedDispatcher
 import android.app.ActivityManager
-import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
@@ -11,18 +10,11 @@ import androidx.compose.animation.ExperimentalAnimationApi
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.runtime.CompositionLocalProvider
-import androidx.datastore.core.DataStore
-import androidx.datastore.preferences.core.Preferences
-import androidx.datastore.preferences.preferencesDataStore
 import coil.annotation.ExperimentalCoilApi
 import com.google.accompanist.permissions.ExperimentalPermissionsApi
 import dagger.hilt.android.AndroidEntryPoint
 import net.teamof.whisper.screens.MainScreen
 import net.teamof.whisper.services.MessageListenerService
-
-
-val Context.dataStore: DataStore<Preferences>
-        by preferencesDataStore(name = "datastore")
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
