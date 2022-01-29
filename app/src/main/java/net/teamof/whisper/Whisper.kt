b2c7dc5c897474b0ced99d0dde15af13d741a4c2
Secret Keeper <shervin.ourfamily@gmail.com>
2022-01-29 19:30:09 +0330

    init custom config WorkManager instance

--- app/src/main/java/net/teamof/whisper/Whisper.kt
+++ app/src/main/java/net/teamof/whisper/Whisper.kt
@@ -1,12 +1,20 @@
 package net.teamof.whisper
 
 import android.app.Application
+import android.util.Log
+import androidx.hilt.work.HiltWorkerFactory
+import androidx.work.Configuration
 import dagger.hilt.android.HiltAndroidApp
 import io.objectbox.android.AndroidObjectBrowser
 import timber.log.Timber
+import javax.inject.Inject
 
 @HiltAndroidApp
-class Whisper : Application() {
+class Whisper : Application(), Configuration.Provider {
+
+    @Inject
+    lateinit var workerFactory: HiltWorkerFactory
+
     override fun onCreate() {
         super.onCreate()
         ObjectBox.init(this)
@@ -17,5 +25,13 @@ class Whisper : Application() {
             val started = AndroidObjectBrowser(ObjectBox.store).start(this)
             Timber.i("Started: $started")
         }
+    }
+
+    override fun getWorkManagerConfiguration(): Configuration {
+
+        return Configuration.Builder()
+            .setWorkerFactory(workerFactory)
+            .setMinimumLoggingLevel(Log.INFO)
+            .build()
     }
 }
