ba6293cd1f382e86885f72f6b681cbdfc5d9713e
Secret Keeper <shervin.ourfamily@gmail.com>
2022-02-16 16:43:16 +0330

    implement updateAssignedMessage method

--- app/src/main/java/net/teamof/whisper/repositories/MessageRepository.kt
+++ app/src/main/java/net/teamof/whisper/repositories/MessageRepository.kt
@@ -3,6 +3,7 @@ package net.teamof.whisper.repositories
 import io.objectbox.Box
 import net.teamof.whisper.ObjectBox
 import net.teamof.whisper.models.Message
+import net.teamof.whisper.models.Message_
 
 class MessageRepository {
 
@@ -10,4 +11,17 @@ class MessageRepository {
 
     fun create(message: Message) = messageBox.put((message))
 
+    fun updateAssignedMessage(message: Message) {
+        val query = messageBox.query().equal(Message_.local_id, message.local_id).build()
+        val result = query.findFirst()
+
+        if (result != null) {
+            val uQuery = messageBox.get(result.local_id)
+            uQuery.id = message.id
+            uQuery.created_at = message.created_at
+            messageBox.put(uQuery)
+        }
+
+        query.close()
+    }
 }
