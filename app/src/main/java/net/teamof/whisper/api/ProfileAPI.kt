08c921a8e2bb8a1777137af1b90d1e7cad3b9a80
Secret Keeper <shervin.ourfamily@gmail.com>
2022-03-13 13:36:40 +0330

    init

---
+++ app/src/main/java/net/teamof/whisper/api/ProfileAPI.kt
@@ -0,0 +1,23 @@
+package net.teamof.whisper.api
+
+import com.squareup.moshi.JsonClass
+import retrofit2.http.GET
+import retrofit2.http.Headers
+import retrofit2.http.Path
+
+@JsonClass(generateAdapter = true)
+data class ProfileAPIResponse(
+    val id: Long,
+    val user_id: Long,
+    val status: String,
+    val description: String,
+    val created_at: String,
+    val updated_at: String?,
+    val deleted_at: String?,
+)
+
+interface ProfileAPI {
+    @GET("profiles/{id}")
+    @Headers("Content-Type: application/json")
+    suspend fun getProfileByID(@Path("id") id: Long): ProfileAPIResponse
+}
