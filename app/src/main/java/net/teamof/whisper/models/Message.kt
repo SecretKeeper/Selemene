7398807d8226c98e7ffbf82e6d62d73d90a6f974
Secret Keeper <shervin.ourfamily@gmail.com>
2022-04-02 14:46:43 +0430

    add verify_delivery_report Boolean

--- app/src/main/java/net/teamof/whisper/models/Message.kt
+++ app/src/main/java/net/teamof/whisper/models/Message.kt
@@ -15,6 +15,9 @@ data class Message(
     var user_id: Long = 0,
     var to_user_id: Long = 0,
     var content: String = "",
+    // this is not for first or second tick , this is just a helper to prevent sending request
+    // server for 
+    var verify_delivery_report: Boolean = false,
     var created_at: Date? = null,
     var updated_at: Date? = null,
 ) : Serializable
