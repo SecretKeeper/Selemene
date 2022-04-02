3c8d64c29dd81ed7ab6cf479577b209e71e0c7a7
Secret Keeper <shervin.ourfamily@gmail.com>
2022-04-02 14:57:38 +0430

    add onNewMessagesArrayListener method to handle new messages

--- app/src/main/java/net/teamof/whisper/sockets/SocketBroadcastListener.kt
+++ app/src/main/java/net/teamof/whisper/sockets/SocketBroadcastListener.kt
@@ -9,8 +9,10 @@ import androidx.core.app.NotificationCompat
 import com.squareup.moshi.JsonAdapter
 import com.squareup.moshi.Moshi
 import net.teamof.whisper.R
+import net.teamof.whisper.models.DeliveryReport
 import net.teamof.whisper.models.Message
 import net.teamof.whisper.models.MessageSide
+import net.teamof.whisper.models.MessagesArray
 import net.teamof.whisper.repositories.ConversationRepository
 import net.teamof.whisper.repositories.MessageRepository
 import timber.log.Timber
@@ -24,6 +26,12 @@ class SocketBroadcastListener @Inject constructor(
 ) {
 
     val messageAdapter: JsonAdapter<Message> = moshi.adapter(Message::class.java)
+
+    val arrayMessageAdapter: JsonAdapter<MessagesArray> =
+        moshi.adapter(MessagesArray::class.java)
+
+    val deliveryReportAdapter: JsonAdapter<DeliveryReport> =
+        moshi.adapter(DeliveryReport::class.java)
 
     fun isAppRunning(context: Context, packageName: String): Boolean {
         val activityManager = context.getSystemService(Context.ACTIVITY_SERVICE) as ActivityManager
@@ -89,6 +97,49 @@ class SocketBroadcastListener @Inject constructor(
                                 it
                             )
                         )
+                    }
+                }
+            }
+        }
+
+    fun onNewMessagesArrayListener(): Socket.OnEventResponseListener =
+        object : Socket.OnEventResponseListener() {
+            override fun onMessage(event: String?, data: String?) {
+                Timber.e("OHHHOHHHHOOHO: $data")
+                arrayMessageAdapter.fromJson(data)?.let { it ->
+
+                    application.sendBroadcast(
+                        Intent("SEND_DELIVERY_REPORT").putExtra(
+                            "DELIVERY_REPORT_MODEL",
+                            DeliveryReport(ids = it.messages.map { message -> message.id })
+                        )
+                    )
+
+                    if (!isAppRunning(application, "net.teamof.whisper")) {
+                        messageRepository.create(it.messages)
+
+                        it.messages.map { message ->
+                            val builder =
+                                NotificationCompat.Builder(application, "MYWhisperCHANNEL_ID")
+                                    .setSmallIcon(R.drawable.objectbox_notification)
+                                    .setContentTitle("Whisper Messenger")
+                                    .setContentText(message.content)
+                                    .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+
+                            val notificationManager =
+                                application.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+                            notificationManager.notify(message.user_id.toInt(), builder.build())
+                        }
+
+                    } else {
+                        it.messages.map { message ->
+                            application.sendBroadcast(
+                                Intent("WhisperLocalMessageCommunication").putExtra(
+                                    "RECEIVE_MESSAGE",
+                                    message
+                                )
+                            )
+                        }
                     }
                 }
             }
