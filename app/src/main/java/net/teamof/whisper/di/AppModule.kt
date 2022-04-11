5e25e3ca45156518ae213f77b47f0d276f45d08a
Secret Keeper <shervin.ourfamily@gmail.com>
2022-04-11 13:31:23 +0430

    add userRepository to di

--- app/src/main/java/net/teamof/whisper/di/AppModule.kt
+++ app/src/main/java/net/teamof/whisper/di/AppModule.kt
@@ -18,6 +18,7 @@ import net.teamof.whisper.models.OBKeyValue_
 import net.teamof.whisper.repositories.ConversationRepository
 import net.teamof.whisper.repositories.KeyValueRepository
 import net.teamof.whisper.repositories.MessageRepository
+import net.teamof.whisper.repositories.UserRepository
 import net.teamof.whisper.sockets.Socket
 import net.teamof.whisper.sockets.SocketBroadcastListener
 import net.teamof.whisper.utils.DateMoshiAdapter
@@ -33,114 +34,118 @@ import javax.inject.Singleton
 @InstallIn(SingletonComponent::class)
 class AppModule {
 
-    @Singleton
-    @Provides
-    fun provideGetUserToken(): String {
-        val obKeyValueBox = ObjectBox.store.boxFor(OBKeyValue::class.java)
-        val query = obKeyValueBox.query(OBKeyValue_.key equal "accessToken").build()
-        val result = query.findFirst()
+	@Singleton
+	@Provides
+	fun provideGetUserToken(): String {
+		val obKeyValueBox = ObjectBox.store.boxFor(OBKeyValue::class.java)
+		val query = obKeyValueBox.query(OBKeyValue_.key equal "accessToken").build()
+		val result = query.findFirst()
 
-        query.close()
-        return result?.value ?: ""
-    }
+		query.close()
+		return result?.value ?: ""
+	}
 
-    private val baseGatewayAddress = "http://10.0.2.2:3333"
+	private val baseGatewayAddress = "http://10.0.2.2:3333"
 
-    private val baseWhisperAddress = "http://10.0.2.2:3335"
+	private val baseWhisperAddress = "http://10.0.2.2:3335"
 
-    @Provides
-    fun providesApplication(@ApplicationContext context: Context): Whisper {
-        return context as Whisper
-    }
+	@Provides
+	fun providesApplication(@ApplicationContext context: Context): Whisper {
+		return context as Whisper
+	}
 
-    @Singleton
-    @Provides
-    fun provideMoshi(): Moshi {
-        return Moshi.Builder().add(Date::class.java, DateMoshiAdapter().nullSafe())
-            .addLast(KotlinJsonAdapterFactory())
-            .build()
-    }
+	@Singleton
+	@Provides
+	fun provideMoshi(): Moshi {
+		return Moshi.Builder().add(Date::class.java, DateMoshiAdapter().nullSafe())
+			.addLast(KotlinJsonAdapterFactory())
+			.build()
+	}
 
-    @Provides
-    fun provideHttpClient(keyValueRepository: KeyValueRepository): OkHttpClient {
-        val logging = HttpLoggingInterceptor()
-        logging.setLevel(HttpLoggingInterceptor.Level.BASIC)
+	@Provides
+	fun provideHttpClient(keyValueRepository: KeyValueRepository): OkHttpClient {
+		val logging = HttpLoggingInterceptor()
+		logging.setLevel(HttpLoggingInterceptor.Level.BODY)
 
-        return OkHttpClient.Builder()
-            .addInterceptor(logging)
-            .addInterceptor(UserTokenInterceptor(keyValueRepository))
-            .build()
-    }
+		return OkHttpClient.Builder()
+			.addInterceptor(logging)
+			.addInterceptor(UserTokenInterceptor(keyValueRepository))
+			.build()
+	}
 
-    @Singleton
-    @Provides
-    fun provideRetrofit(okHttpClient: OkHttpClient): Retrofit = Retrofit.Builder()
-        .addConverterFactory(MoshiConverterFactory.create())
-        .baseUrl(baseWhisperAddress)
-        .client(okHttpClient)
-        .build()
+	@Singleton
+	@Provides
+	fun provideRetrofit(okHttpClient: OkHttpClient): Retrofit = Retrofit.Builder()
+		.addConverterFactory(MoshiConverterFactory.create())
+		.baseUrl(baseWhisperAddress)
+		.client(okHttpClient)
+		.build()
 
-    @Singleton
-    @Provides
-    @Named("Gateway")
-    fun provideGatewayRetrofit(okHttpClient: OkHttpClient): Retrofit = Retrofit.Builder()
-        .addConverterFactory(MoshiConverterFactory.create())
-        .baseUrl(baseGatewayAddress)
-        .client(okHttpClient)
-        .build()
+	@Singleton
+	@Provides
+	@Named("Gateway")
+	fun provideGatewayRetrofit(okHttpClient: OkHttpClient): Retrofit = Retrofit.Builder()
+		.addConverterFactory(MoshiConverterFactory.create())
+		.baseUrl(baseGatewayAddress)
+		.client(okHttpClient)
+		.build()
 
-    @Singleton
-    @Provides
-    fun provideWhisperSocket(getUserToken: String): Socket {
-        return Socket.Builder.with("ws://10.0.2.2:3335/ws/")
-            .addHeader("Authorization", "Bearer $getUserToken")
-            .build().connect()
-    }
+	@Singleton
+	@Provides
+	fun provideWhisperSocket(getUserToken: String): Socket {
+		return Socket.Builder.with("ws://10.0.2.2:3335/ws/")
+			.addHeader("Authorization", "Bearer $getUserToken")
+			.build().connect()
+	}
 
-    @Singleton
-    @Provides
-    fun provideAuthAPI(@Named("Gateway") retrofit: Retrofit): AuthAPI =
-        retrofit.create(AuthAPI::class.java)
+	@Singleton
+	@Provides
+	fun provideAuthAPI(@Named("Gateway") retrofit: Retrofit): AuthAPI =
+		retrofit.create(AuthAPI::class.java)
 
-    @Singleton
-    @Provides
-    fun provideSearchAPI(retrofit: Retrofit): SearchAPI = retrofit.create(SearchAPI::class.java)
+	@Singleton
+	@Provides
+	fun provideSearchAPI(retrofit: Retrofit): SearchAPI = retrofit.create(SearchAPI::class.java)
 
-    @Singleton
-    @Provides
-    fun provideUsersAPI(retrofit: Retrofit): UsersAPI = retrofit.create(UsersAPI::class.java)
+	@Singleton
+	@Provides
+	fun provideUsersAPI(retrofit: Retrofit): UsersAPI = retrofit.create(UsersAPI::class.java)
 
-    @Singleton
-    @Provides
-    fun provideAccountAPI(retrofit: Retrofit): AccountAPI = retrofit.create(AccountAPI::class.java)
+	@Singleton
+	@Provides
+	fun provideAccountAPI(retrofit: Retrofit): AccountAPI = retrofit.create(AccountAPI::class.java)
 
-    @Singleton
-    @Provides
-    fun provideProfileAPI(retrofit: Retrofit): ProfileAPI = retrofit.create(ProfileAPI::class.java)
+	@Singleton
+	@Provides
+	fun provideProfileAPI(retrofit: Retrofit): ProfileAPI = retrofit.create(ProfileAPI::class.java)
 
-    @Singleton
-    @Provides
-    fun provideMessageRepository() = MessageRepository()
+	@Singleton
+	@Provides
+	fun provideUserRepository() = UserRepository()
 
-    @Singleton
-    @Provides
-    fun provideKeyValueRepository() = KeyValueRepository()
+	@Singleton
+	@Provides
+	fun provideMessageRepository() = MessageRepository()
 
-    @Singleton
-    @Provides
-    fun provideConversationRepository(usersAPI: UsersAPI) = ConversationRepository(usersAPI)
+	@Singleton
+	@Provides
+	fun provideKeyValueRepository() = KeyValueRepository()
 
-    @Singleton
-    @Provides
-    fun provideSocketBroadcastListener(
-        application: Application,
-        moshi: Moshi,
-        messageRepository: MessageRepository,
-        conversationRepository: ConversationRepository
-    ) = SocketBroadcastListener(
-        application,
-        moshi,
-        messageRepository,
-        conversationRepository
-    )
+	@Singleton
+	@Provides
+	fun provideConversationRepository(usersAPI: UsersAPI) = ConversationRepository(usersAPI)
+
+	@Singleton
+	@Provides
+	fun provideSocketBroadcastListener(
+		application: Application,
+		moshi: Moshi,
+		messageRepository: MessageRepository,
+		conversationRepository: ConversationRepository
+	) = SocketBroadcastListener(
+		application,
+		moshi,
+		messageRepository,
+		conversationRepository
+	)
 }
