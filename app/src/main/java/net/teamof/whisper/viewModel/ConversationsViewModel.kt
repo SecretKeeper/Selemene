167314164de4fa397d01ef294d48ec752c90f04b
Secret Keeper <shervin.ourfamily@gmail.com>
2022-04-11 13:31:04 +0430

    defined periodic worker to keep user data update

--- app/src/main/java/net/teamof/whisper/viewModel/ConversationsViewModel.kt
+++ app/src/main/java/net/teamof/whisper/viewModel/ConversationsViewModel.kt
@@ -1,6 +1,8 @@
 package net.teamof.whisper.viewModel
 
-import androidx.lifecycle.ViewModel
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.work.*
 import dagger.hilt.android.lifecycle.HiltViewModel
 import io.objectbox.Box
 import io.objectbox.android.AndroidScheduler
@@ -10,38 +12,62 @@ import net.teamof.whisper.ObjectBox
 import net.teamof.whisper.models.Conversation
 import net.teamof.whisper.models.Conversation_
 import net.teamof.whisper.repositories.ConversationRepository
+import net.teamof.whisper.workers.CheckForNewProfilePhotos
 import timber.log.Timber
+import java.util.concurrent.TimeUnit
 import javax.inject.Inject
 
 @HiltViewModel
 class ConversationsViewModel @Inject constructor(
-    private val conversationRepository: ConversationRepository
-) : ViewModel() {
+	application: Application,
+	private val conversationRepository: ConversationRepository
+) : AndroidViewModel(application) {
 
-    private val conversationBox: Box<Conversation> =
-        ObjectBox.store.boxFor(Conversation::class.java)
+	private val workManager: WorkManager = WorkManager.getInstance(application)
 
-    private var _conversations: ObjectBoxLiveData<Conversation> =
-        ObjectBoxLiveData(fetchAndObserveConversations())
+	private val conversationBox: Box<Conversation> =
+		ObjectBox.store.boxFor(Conversation::class.java)
 
-    val conversations: ObjectBoxLiveData<Conversation> = _conversations
+	private var _conversations: ObjectBoxLiveData<Conversation> =
+		ObjectBoxLiveData(fetchAndObserveConversations())
 
-    private fun fetchAndObserveConversations(): Query<Conversation>? {
-        val query = conversationBox.query().build()
+	val conversations: ObjectBoxLiveData<Conversation> = _conversations
 
-        query.subscribe().on(AndroidScheduler.mainThread()).observer {
-            Timber.d("CONVERSATIONS UPDATED!!! WTF ")
-        }
+	private fun fetchAndObserveConversations(): Query<Conversation>? {
+		val query = conversationBox.query().build()
 
-        return query
-    }
+		query.subscribe().on(AndroidScheduler.mainThread()).observer {
+			Timber.d("CONVERSATIONS UPDATED!!! WTF ")
+		}
 
-    fun createConversation(conversation: Conversation) =
-        conversationRepository.create(conversation)
+		val constraints = Constraints.Builder()
+			.setRequiredNetworkType(NetworkType.CONNECTED)
+			.build()
 
-    fun getConversation(to_user_id: Long): Conversation? =
-        conversationBox.query().run {
-            equal(Conversation_.to_user_id, to_user_id)
-            build()
-        }.use { it.findFirst() }
+		val builder = Data.Builder()
+		builder.putLongArray("usersIdsToFetch", conversationRepository.getAllUsers())
+
+		val revokeTokenRequest =
+			PeriodicWorkRequestBuilder<CheckForNewProfilePhotos>(15, TimeUnit.MINUTES)
+				.setConstraints(constraints)
+				.setInputData(builder.build())
+				.build()
+
+		workManager.enqueueUniquePeriodicWork(
+			"UPDATE_USER_DATA",
+			ExistingPeriodicWorkPolicy.REPLACE,
+			revokeTokenRequest
+		)
+		
+		return query
+	}
+
+	fun createConversation(conversation: Conversation) =
+		conversationRepository.create(conversation)
+
+	fun getConversation(to_user_id: Long): Conversation? =
+		conversationBox.query().run {
+			equal(Conversation_.to_user_id, to_user_id)
+			build()
+		}.use { it.findFirst() }
 }
