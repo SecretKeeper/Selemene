da2c88950161601b5f300788032b610cf06284fc
Secret Keeper <shervin.ourfamily@gmail.com>
2022-04-11 13:20:52 +0430

    add UserAPIWithoutCounters data class

--- app/src/main/java/net/teamof/whisper/models/UserAPI.kt
+++ app/src/main/java/net/teamof/whisper/models/UserAPI.kt
@@ -4,10 +4,19 @@ import com.squareup.moshi.JsonClass
 
 @JsonClass(generateAdapter = true)
 data class UserAPI(
-    val user_id: Long,
-    val username: String,
-    val email: String,
-    val avatar: String?,
-    val profile: Profile,
-    val _count: Counters? // make it optional for now but in next steps must implement each service follower for own
+	val user_id: Long,
+	val username: String,
+	val email: String,
+	val avatar: String?,
+	val profile: Profile,
+	val _count: Counters? // make it optional for now but in next steps must implement each service follower for own
+)
+
+@JsonClass(generateAdapter = true)
+data class UserAPIWithoutCounters(
+	val user_id: Long,
+	val username: String,
+	val email: String,
+	val avatar: String?,
+	val profile: Profile,
 )
