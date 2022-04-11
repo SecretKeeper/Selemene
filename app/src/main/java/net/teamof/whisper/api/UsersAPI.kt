e38d3e297287c6e119d445021caec08080e30630
Secret Keeper <shervin.ourfamily@gmail.com>
2022-04-11 13:20:17 +0430

    add getMultipleUsers API

--- app/src/main/java/net/teamof/whisper/api/UsersAPI.kt
+++ app/src/main/java/net/teamof/whisper/api/UsersAPI.kt
@@ -1,13 +1,22 @@
 package net.teamof.whisper.api
 
+import com.squareup.moshi.JsonClass
 import net.teamof.whisper.models.UserAPI
+import net.teamof.whisper.models.UserAPIWithoutCounters
 import retrofit2.Call
-import retrofit2.http.GET
-import retrofit2.http.Headers
-import retrofit2.http.Path
+import retrofit2.http.*
+
+@JsonClass(generateAdapter = true)
+data class GetMultipleUsers(
+	val ids: List<Long>
+)
 
 interface UsersAPI {
-    @GET("users/id/{id}")
-    @Headers("Content-Type: application/json")
-    fun getUserProfile(@Path("id") user_id: String): Call<UserAPI>
+	@GET("users/id/{id}")
+	@Headers("Content-Type: application/json")
+	fun getUserProfile(@Path("id") user_id: String): Call<UserAPI>
+
+	@POST("users/multiple")
+	@Headers("Content-Type: application/json")
+	fun getMultipleUsers(@Body ids: GetMultipleUsers): Call<List<UserAPIWithoutCounters>>
 }
