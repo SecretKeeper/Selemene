0e66a1b7238cd6a1057a29a05d8c7288e90269ee
Secret Keeper <shervin.ourfamily@gmail.com>
2022-04-15 18:48:45 +0430

    init

---
+++ app/src/main/java/net/teamof/whisper/workers/UpdateProfilePhoto.kt
@@ -0,0 +1,73 @@
+package net.teamof.whisper.workers
+
+import android.content.Context
+import android.webkit.MimeTypeMap
+import androidx.core.net.toUri
+import androidx.hilt.work.HiltWorker
+import androidx.work.CoroutineWorker
+import androidx.work.WorkerParameters
+import dagger.assisted.Assisted
+import dagger.assisted.AssistedInject
+import net.teamof.whisper.api.UsersAPI
+import net.teamof.whisper.repositories.ConversationRepository
+import net.teamof.whisper.repositories.UserRepository
+import okhttp3.MediaType.Companion.toMediaType
+import okhttp3.MultipartBody
+import okhttp3.RequestBody
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+
+@HiltWorker
+class UpdateProfilePhoto @AssistedInject constructor(
+	@Assisted appContext: Context,
+	@Assisted workerParams: WorkerParameters,
+	private val usersAPI: UsersAPI,
+	private val userRepository: UserRepository,
+	private val conversationRepository: ConversationRepository
+) :
+	CoroutineWorker(appContext, workerParams) {
+	override suspend fun doWork(): Result {
+		return try {
+			val avatarUriString = inputData.getString("avatar_uri")!!
+
+			val imageFileStream =
+				applicationContext.contentResolver.openInputStream(
+					avatarUriString.toUri()
+				)
+
+			val mime = MimeTypeMap.getSingleton()
+			val type = mime.getExtensionFromMimeType(
+				applicationContext.contentResolver.getType(avatarUriString.toUri())
+			)
+
+			val part = MultipartBody.Part.createFormData(
+				"avatar", "myPic.${type}", RequestBody.create(
+					"image/${type}".toMediaType(),
+					imageFileStream!!.readBytes()
+				)
+			)
+			usersAPI.setAvatar(part)
+
+			val response = usersAPI.setAvatar(part)
+
+			response.enqueue(object : Callback<Void> {
+				override fun onResponse(
+					call: Call<Void>,
+					response: Response<Void>
+				) {
+					Result.success()
+				}
+
+				override fun onFailure(call: Call<Void>, t: Throwable) {
+					Result.retry()
+				}
+			})
+
+			Result.success()
+
+		} catch (throwable: Throwable) {
+			Result.retry()
+		}
+	}
+}
