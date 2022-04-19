1d1906852877c68ca82673d0553dffca9a072f7d
Secret Keeper <shervin.ourfamily@gmail.com>
2022-04-19 17:06:02 +0430

    add setAvatar UserAPI

--- app/src/main/java/net/teamof/whisper/api/UsersAPI.kt
+++ app/src/main/java/net/teamof/whisper/api/UsersAPI.kt
@@ -3,6 +3,7 @@ package net.teamof.whisper.api
 import com.squareup.moshi.JsonClass
 import net.teamof.whisper.models.UserAPI
 import net.teamof.whisper.models.UserAPIWithoutCounters
+import okhttp3.MultipartBody
 import retrofit2.Call
 import retrofit2.http.*
 
@@ -19,4 +20,11 @@ interface UsersAPI {
 	@POST("users/multiple")
 	@Headers("Content-Type: application/json")
 	fun getMultipleUsers(@Body ids: GetMultipleUsers): Call<List<UserAPIWithoutCounters>>
+
+	@Multipart
+	@POST("user/set-avatar")
+	fun setAvatar(
+		@Part() avatar: MultipartBody.Part,
+		@Header("Authorization") token: String
+	): Call<Void>
 }
