6af51ba2f79b2964a7c915623d3f3da711ada697
Secret Keeper <shervin.ourfamily@gmail.com>
2022-04-23 15:22:13 +0430

    defined setAvatar method

--- app/src/main/java/net/teamof/whisper/repositories/KeyValueRepository.kt
+++ app/src/main/java/net/teamof/whisper/repositories/KeyValueRepository.kt
@@ -7,79 +7,91 @@ import net.teamof.whisper.models.OBKeyValue
 import net.teamof.whisper.models.OBKeyValue_
 
 class KeyValueRepository {
-    private val oBKeyValueBox: Box<OBKeyValue> =
-        ObjectBox.store.boxFor(OBKeyValue::class.java)
+	private val oBKeyValueBox: Box<OBKeyValue> =
+		ObjectBox.store.boxFor(OBKeyValue::class.java)
 
-    fun create(pair: OBKeyValue) {
-        oBKeyValueBox.put(pair)
-    }
+	fun create(pair: OBKeyValue) {
+		oBKeyValueBox.put(pair)
+	}
 
-    fun createOrUpdate(pair: OBKeyValue) {
-        val query = oBKeyValueBox.query(OBKeyValue_.key equal pair.key).build()
-        val result = query.findFirst()
+	fun createOrUpdate(pair: OBKeyValue) {
+		val query = oBKeyValueBox.query(OBKeyValue_.key equal pair.key).build()
+		val result = query.findFirst()
 
-        if (result != null) {
-            val uQuery = oBKeyValueBox.get(result.id)
-            uQuery.value = pair.value
-            oBKeyValueBox.put(uQuery)
-        } else
-            oBKeyValueBox.put(pair)
-    }
+		if (result != null) {
+			val uQuery = oBKeyValueBox.get(result.id)
+			uQuery.value = pair.value
+			oBKeyValueBox.put(uQuery)
+		} else
+			oBKeyValueBox.put(pair)
+	}
 
-    fun createOrUpdate(pairs: List<OBKeyValue>) {
+	fun createOrUpdate(pairs: List<OBKeyValue>) {
 
-        pairs.map { pair ->
-            val query = oBKeyValueBox.query(OBKeyValue_.key equal pair.key).build()
-            val result = query.findFirst()
+		pairs.map { pair ->
+			val query = oBKeyValueBox.query(OBKeyValue_.key equal pair.key).build()
+			val result = query.findFirst()
 
-            if (result != null) {
-                val uQuery = oBKeyValueBox.get(result.id)
-                uQuery.value = pair.value
-                oBKeyValueBox.put(uQuery)
-            } else
-                oBKeyValueBox.put(pair)
-        }
-    }
+			if (result != null) {
+				val uQuery = oBKeyValueBox.get(result.id)
+				uQuery.value = pair.value
+				oBKeyValueBox.put(uQuery)
+			} else
+				oBKeyValueBox.put(pair)
+		}
+	}
 
-    fun getLoggedUser(): Long {
-        val query = oBKeyValueBox.query(OBKeyValue_.key equal "user_id").build()
-        val result = query.findFirst()
-        query.close()
+	fun getLoggedUser(): Long {
+		val query = oBKeyValueBox.query(OBKeyValue_.key equal "user_id").build()
+		val result = query.findFirst()
+		query.close()
 
-        return result?.value?.toLong() ?: 0L
-    }
+		return result?.value?.toLong() ?: 0L
+	}
 
-    fun getPair(key: String): OBKeyValue? {
-        val query = oBKeyValueBox.query(OBKeyValue_.key equal key).build()
-        val result = query.findFirst()
-        query.close()
+	fun getPair(key: String): OBKeyValue? {
+		val query = oBKeyValueBox.query(OBKeyValue_.key equal key).build()
+		val result = query.findFirst()
+		query.close()
 
-        return result
-    }
+		return result
+	}
 
-    fun getToken(): String {
-        val query = oBKeyValueBox.query(OBKeyValue_.key equal "accessToken").build()
-        val result = query.findFirst()
-        query.close()
+	fun getToken(): String {
+		val query = oBKeyValueBox.query(OBKeyValue_.key equal "accessToken").build()
+		val result = query.findFirst()
+		query.close()
 
-        return result?.value ?: ""
-    }
+		return result?.value ?: ""
+	}
 
-    fun delete(key: String) {
-        val query = oBKeyValueBox.query(OBKeyValue_.key equal key).build()
-        query.remove()
+	fun setAvatar(avatar: String) {
+		val query = oBKeyValueBox.query(OBKeyValue_.key equal "avatar").build()
+		val result = query.findFirst()
 
-        query.close()
-    }
+		if (result != null) {
+			val uQuery = oBKeyValueBox.get(result.id)
+			uQuery.value = avatar
+			oBKeyValueBox.put(uQuery)
+		} else
+			oBKeyValueBox.put(OBKeyValue(key = "avatar", value = avatar))
+	}
 
-    fun delete(keys: List<String>) {
+	fun delete(key: String) {
+		val query = oBKeyValueBox.query(OBKeyValue_.key equal key).build()
+		query.remove()
 
-        keys.map { key ->
-            val query = oBKeyValueBox.query(OBKeyValue_.key equal key).build()
-            query.remove()
+		query.close()
+	}
 
-            query.close()
-        }
+	fun delete(keys: List<String>) {
 
-    }
+		keys.map { key ->
+			val query = oBKeyValueBox.query(OBKeyValue_.key equal key).build()
+			query.remove()
+
+			query.close()
+		}
+
+	}
 }
