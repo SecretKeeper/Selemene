a0408a48b8328127f8f3d291d16cb28a572928b1
Secret Keeper <shervin.ourfamily@gmail.com>
2022-04-29 20:44:19 +0430

    added red alert n green color codes

--- app/src/main/java/net/teamof/whisper/ui/theme/Color.kt
+++ app/src/main/java/net/teamof/whisper/ui/theme/Color.kt
@@ -6,3 +6,8 @@ val Purple200 = Color(0xFFBB86FC)
 val Purple500 = Color(0xFF6200EE)
 val Purple700 = Color(0xFF3700B3)
 val Teal200 = Color(0xFF03DAC5)
+val AccentGreen = Color(0xff3ddc84)
+val RedAlertColor = Color(0xfff50537)
+
+
+const val AccentGreenLong = 0xff3ddc84
