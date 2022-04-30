ca5751534a0968c4466d53de44652382d10f0aaf
Secret Keeper <shervin.ourfamily@gmail.com>
2022-04-30 20:48:49 +0430

    added setStatus method for retrofit

--- app/src/main/java/net/teamof/whisper/api/ProfileAPI.kt
+++ app/src/main/java/net/teamof/whisper/api/ProfileAPI.kt
@@ -1,9 +1,9 @@
 package net.teamof.whisper.api
 
 import com.squareup.moshi.JsonClass
-import retrofit2.http.GET
-import retrofit2.http.Headers
-import retrofit2.http.Path
+import okhttp3.ResponseBody
+import retrofit2.Response
+import retrofit2.http.*
 
 @JsonClass(generateAdapter = true)
 data class ProfileAPIResponse(
@@ -16,8 +16,18 @@ data class ProfileAPIResponse(
 	val deleted_at: String?,
 )
 
+@JsonClass(generateAdapter = true)
+data class SetStatus(
+	val sender: Long = 0,
+	val status: String
+)
+
 interface ProfileAPI {
 	@GET("profiles/{id}")
 	@Headers("Content-Type: application/json")
 	suspend fun getProfileByID(@Path("id") id: Long): ProfileAPIResponse
+
+	@POST("profile/set-status")
+	@Headers("Content-Type: application/json")
+	suspend fun setStatus(@Body setStatus: SetStatus): Response<ResponseBody>
 }
