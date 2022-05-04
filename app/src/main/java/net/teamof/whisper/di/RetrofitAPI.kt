2c3b56e80f535f8c2ec9901a395847f030c8ad0a
Secret Keeper <shervin.ourfamily@gmail.com>
2022-05-04 15:36:23 +0430

    init

---
+++ app/src/main/java/net/teamof/whisper/di/RetrofitAPI.kt
@@ -0,0 +1,37 @@
+package net.teamof.whisper.di
+
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import net.teamof.whisper.api.*
+import retrofit2.Retrofit
+import javax.inject.Named
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+class RetrofitAPI {
+	@Singleton
+	@Provides
+	fun provideAuthAPI(@Named("Gateway") retrofit: Retrofit): AuthAPI =
+		retrofit.create(AuthAPI::class.java)
+
+	@Singleton
+	@Provides
+	fun provideSearchAPI(retrofit: Retrofit): SearchAPI = retrofit.create(SearchAPI::class.java)
+
+	@Singleton
+	@Provides
+	fun provideUsersAPI(retrofit: Retrofit): UsersAPI = retrofit.create(UsersAPI::class.java)
+
+	@Singleton
+	@Provides
+	fun provideAccountAPI(@Named("Gateway") retrofit: Retrofit): AccountAPI =
+		retrofit.create(AccountAPI::class.java)
+
+	@Singleton
+	@Provides
+	fun provideProfileAPI(retrofit: Retrofit): ProfileAPI = retrofit.create(ProfileAPI::class.java)
+
+}
