db0cb768b0ce6566331fd5c8aee92e68a79366d8
Secret Keeper <shervin.ourfamily@gmail.com>
2022-05-04 15:36:39 +0430

    broken to multiple di components

--- app/src/main/java/net/teamof/whisper/di/AppModule.kt
+++ app/src/main/java/net/teamof/whisper/di/AppModule.kt
@@ -12,13 +12,12 @@ import dagger.hilt.components.SingletonComponent
 import io.objectbox.kotlin.equal
 import net.teamof.whisper.ObjectBox
 import net.teamof.whisper.Whisper
-import net.teamof.whisper.api.*
+import net.teamof.whisper.api.UserTokenInterceptor
 import net.teamof.whisper.models.OBKeyValue
 import net.teamof.whisper.models.OBKeyValue_
 import net.teamof.whisper.repositories.ConversationRepository
 import net.teamof.whisper.repositories.KeyValueRepository
 import net.teamof.whisper.repositories.MessageRepository
-import net.teamof.whisper.repositories.UserRepository
 import net.teamof.whisper.sockets.Socket
 import net.teamof.whisper.sockets.SocketBroadcastListener
 import net.teamof.whisper.utils.DateMoshiAdapter
@@ -101,45 +100,7 @@ class AppModule {
 			.addHeader("Authorization", "Bearer $getUserToken")
 			.build().connect()
 	}
-
-	@Singleton
-	@Provides
-	fun provideAuthAPI(@Named("Gateway") retrofit: Retrofit): AuthAPI =
-		retrofit.create(AuthAPI::class.java)
-
-	@Singleton
-	@Provides
-	fun provideSearchAPI(retrofit: Retrofit): SearchAPI = retrofit.create(SearchAPI::class.java)
-
-	@Singleton
-	@Provides
-	fun provideUsersAPI(retrofit: Retrofit): UsersAPI = retrofit.create(UsersAPI::class.java)
-
-	@Singleton
-	@Provides
-	fun provideAccountAPI(@Named("Gateway") retrofit: Retrofit): AccountAPI =
-		retrofit.create(AccountAPI::class.java)
-
-	@Singleton
-	@Provides
-	fun provideProfileAPI(retrofit: Retrofit): ProfileAPI = retrofit.create(ProfileAPI::class.java)
-
-	@Singleton
-	@Provides
-	fun provideUserRepository() = UserRepository()
-
-	@Singleton
-	@Provides
-	fun provideMessageRepository() = MessageRepository()
-
-	@Singleton
-	@Provides
-	fun provideKeyValueRepository() = KeyValueRepository()
-
-	@Singleton
-	@Provides
-	fun provideConversationRepository(usersAPI: UsersAPI) = ConversationRepository(usersAPI)
-
+	
 	@Singleton
 	@Provides
 	fun provideSocketBroadcastListener(
