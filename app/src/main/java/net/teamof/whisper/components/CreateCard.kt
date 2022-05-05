fbda9131ca3bbd68bbeb8ee9a5f4ba44093f519c
Secret Keeper <shervin.ourfamily@gmail.com>
2022-05-06 03:50:58 +0430

    format using tab indents

--- app/src/main/java/net/teamof/whisper/components/CreateCard.kt
+++ app/src/main/java/net/teamof/whisper/components/CreateCard.kt
@@ -20,41 +20,41 @@ import net.teamof.whisper.ui.theme.fontFamily
 @ExperimentalMaterialApi
 @Composable
 fun CreateCard(navController: NavController, createCard: CreateCard) {
-    Card(
-        elevation = 0.dp,
-        onClick = { if (createCard.route.isNotEmpty()) navController.navigate("Contacts/Messaging") },
-        modifier = Modifier
-            .fillMaxWidth()
-            .background(Color.White)
-            .padding(bottom = 15.dp),
-    ) {
-        Row(
-            verticalAlignment = Alignment.CenterVertically,
-            modifier = Modifier.padding(vertical = 15.dp, horizontal = 20.dp)
-        ) {
-            Image(
-                painter = painterResource(id = createCard.image),
-                contentDescription = null,
-                Modifier
-                    .width(65.dp)
-                    .height(65.dp)
-                    .padding(end = 25.dp)
-            )
-            Column {
-                Text(
-                    text = createCard.title,
-                    fontSize = 16.sp,
-                    fontFamily = fontFamily,
-                    modifier = Modifier.padding(bottom = 5.dp)
-                )
-                Text(
-                    text = createCard.shortDescription,
-                    fontSize = 13.sp,
-                    lineHeight = 20.sp,
-                    fontFamily = fontFamily,
-                    color = Color.Gray
-                )
-            }
-        }
-    }
+	Card(
+		elevation = 0.dp,
+		onClick = { if (createCard.route.isNotEmpty()) navController.navigate("Contacts/Messaging") },
+		modifier = Modifier
+			.fillMaxWidth()
+			.background(Color.White)
+			.padding(bottom = 15.dp),
+	) {
+		Row(
+			verticalAlignment = Alignment.CenterVertically,
+			modifier = Modifier.padding(vertical = 15.dp, horizontal = 20.dp)
+		) {
+			Image(
+				painter = painterResource(id = createCard.image),
+				contentDescription = null,
+				Modifier
+					.width(65.dp)
+					.height(65.dp)
+					.padding(end = 25.dp)
+			)
+			Column {
+				Text(
+					text = createCard.title,
+					fontSize = 16.sp,
+					fontFamily = fontFamily,
+					modifier = Modifier.padding(bottom = 5.dp)
+				)
+				Text(
+					text = createCard.shortDescription,
+					fontSize = 13.sp,
+					lineHeight = 20.sp,
+					fontFamily = fontFamily,
+					color = Color.Gray
+				)
+			}
+		}
+	}
 }
