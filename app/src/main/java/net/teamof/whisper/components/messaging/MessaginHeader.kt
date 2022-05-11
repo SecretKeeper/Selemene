bc872fedbd432957a8c70b62282c82b4c52acc11
Secret Keeper <shervin.ourfamily@gmail.com>
2022-05-11 09:37:05 +0430

    update coil-compose to v2 API

--- app/src/main/java/net/teamof/whisper/components/messaging/MessaginHeader.kt
+++ app/src/main/java/net/teamof/whisper/components/messaging/MessaginHeader.kt
@@ -2,10 +2,10 @@ package net.teamof.whisper.components.messaging
 
 import android.annotation.SuppressLint
 import androidx.compose.animation.*
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
@@ -13,14 +13,15 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import coil.annotation.ExperimentalCoilApi
-import coil.compose.rememberImagePainter
-import coil.transform.CircleCropTransformation
+import coil.compose.AsyncImage
 import kotlinx.coroutines.DelicateCoroutinesApi
 import net.teamof.whisper.R
 import net.teamof.whisper.models.User
@@ -105,16 +106,14 @@ fun MainHeader(
 					}
 				}
 		) {
-			Image(
-				painter = rememberImagePainter(data = user?.avatar,
-					builder = {
-						transformations(CircleCropTransformation())
-					}
-				),
+			AsyncImage(
+				model = user?.avatar,
 				contentDescription = null,
+				contentScale = ContentScale.Crop,
 				modifier = Modifier
 					.width(50.dp)
 					.height(50.dp)
+					.clip(CircleShape)
 			)
 			Column(
 				Modifier
