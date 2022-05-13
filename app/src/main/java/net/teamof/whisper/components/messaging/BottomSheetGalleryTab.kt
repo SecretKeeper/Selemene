ff0801ab6fc6be338ef435171535f33216a6f255
Secret Keeper <shervin.ourfamily@gmail.com>
2022-05-13 09:15:06 +0430

    update to coil-compose v2 API

--- app/src/main/java/net/teamof/whisper/components/messaging/BottomSheetGalleryTab.kt
+++ app/src/main/java/net/teamof/whisper/components/messaging/BottomSheetGalleryTab.kt
@@ -1,7 +1,6 @@
 package net.teamof.whisper.components.messaging
 
 import androidx.compose.foundation.ExperimentalFoundationApi
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.GridCells
@@ -9,26 +8,25 @@ import androidx.compose.foundation.lazy.LazyVerticalGrid
 import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.unit.dp
 import coil.annotation.ExperimentalCoilApi
-import coil.compose.rememberImagePainter
-import coil.size.Scale
+import coil.compose.AsyncImage
 
 @OptIn(ExperimentalCoilApi::class)
 @ExperimentalFoundationApi
 @Composable
 fun BottomSheetGalleryTab(images: List<GalleryImage>) {
-    LazyVerticalGrid(cells = GridCells.Adaptive(minSize = 130.dp)) {
-        itemsIndexed(images) { _, image ->
-            Image(
-                painter = rememberImagePainter(
-                    data = image.contentUri,
-                    builder = { scale(Scale.FILL) }),
-                contentDescription = null,
-                modifier = Modifier
+	LazyVerticalGrid(cells = GridCells.Adaptive(minSize = 130.dp)) {
+		itemsIndexed(images) { _, image ->
+			AsyncImage(
+				model = image.contentUri,
+				contentScale = ContentScale.FillBounds,
+				contentDescription = null,
+				modifier = Modifier
                     .width(130.dp)
                     .height(130.dp)
-            )
-        }
-    }
+			)
+		}
+	}
 }
