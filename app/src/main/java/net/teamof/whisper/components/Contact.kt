958ea2ef98620c94e3936f66a3f01c53817ba2df
Secret Keeper <shervin.ourfamily@gmail.com>
2022-05-13 09:15:11 +0430

    update to coil-compose v2 API

--- app/src/main/java/net/teamof/whisper/components/Contact.kt
+++ app/src/main/java/net/teamof/whisper/components/Contact.kt
@@ -1,7 +1,7 @@
 package net.teamof.whisper.components
 
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -10,15 +10,16 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import coil.annotation.ExperimentalCoilApi
-import coil.compose.rememberImagePainter
-import coil.transform.CircleCropTransformation
+import coil.compose.AsyncImage
 import net.teamof.whisper.R
 import net.teamof.whisper.models.UserAPI
 import net.teamof.whisper.ui.theme.fontFamily
@@ -28,78 +29,76 @@ import net.teamof.whisper.viewModel.ProfileViewModel
 @ExperimentalMaterialApi
 @Composable
 fun Contact(
-    navController: NavController,
-    profileViewModel: ProfileViewModel,
-    data: UserAPI,
-    action: String
+	navController: NavController,
+	profileViewModel: ProfileViewModel,
+	data: UserAPI,
+	action: String
 ) {
 
-    var checked by rememberSaveable { mutableStateOf(false) }
+	var checked by rememberSaveable { mutableStateOf(false) }
 
-    Card(
-        elevation = 0.dp,
-        modifier = Modifier
-            .fillMaxWidth()
-            .padding(start = 40.dp),
-        onClick = {
-            when (action) {
-                "CreateGroup" -> checked = !checked
-                "Messaging" -> {
-                    profileViewModel.setUserStateByUserID(
-                        data.user_id
-                    ) { navController.navigate("Profile/${data.user_id}") }
-                }
-            }
-        }) {
-        Row(
-            verticalAlignment = Alignment.CenterVertically,
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(vertical = 10.dp, horizontal = 10.dp)
-        ) {
-            Image(
-                painter = rememberImagePainter(data = data.avatar,
-                    builder = {
-                        transformations(CircleCropTransformation())
-                    }
-                ),
-                contentDescription = null,
-                modifier = Modifier
-                    .width(60.dp)
-                    .height(60.dp)
-            )
-            Column(
-                Modifier
-                    .weight(2f)
-                    .padding(start = 15.dp)
-            ) {
-                Row(verticalAlignment = Alignment.Top) {
-                    Text(
-                        text = data.username,
-                        fontFamily = fontFamily,
-                        fontSize = 15.sp,
-                        modifier = Modifier
-                            .padding(bottom = 7.dp)
-                            .weight(1f)
-                    )
-                    if (action == "Messaging")
-                        if (checked) Icon(
-                            imageVector = ImageVector.vectorResource(id = R.drawable.ic_checkmark),
-                            contentDescription = null,
-                            tint = MaterialTheme.colors.primary,
-                            modifier = Modifier
-                                .width(30.dp)
-                                .height(30.dp)
-                                .padding(end = 10.dp)
-                        )
-                }
-                Text(
-                    text = data.profile.status ?: "",
-                    fontFamily = fontFamily,
-                    fontSize = 13.sp,
-                    color = Color.Gray
-                )
-            }
-        }
-    }
+	Card(
+		elevation = 0.dp,
+		modifier = Modifier
+			.fillMaxWidth()
+			.padding(start = 40.dp),
+		onClick = {
+			when (action) {
+				"CreateGroup" -> checked = !checked
+				"Messaging" -> {
+					profileViewModel.setUserStateByUserID(
+						data.user_id
+					) { navController.navigate("Profile/${data.user_id}") }
+				}
+			}
+		}) {
+		Row(
+			verticalAlignment = Alignment.CenterVertically,
+			modifier = Modifier
+				.fillMaxWidth()
+				.padding(vertical = 10.dp, horizontal = 10.dp)
+		) {
+			AsyncImage(
+				model = data.avatar,
+				contentDescription = null,
+				contentScale = ContentScale.Crop,
+				modifier = Modifier
+					.width(60.dp)
+					.height(60.dp)
+					.clip(CircleShape)
+			)
+			Column(
+				Modifier
+					.weight(2f)
+					.padding(start = 15.dp)
+			) {
+				Row(verticalAlignment = Alignment.Top) {
+					Text(
+						text = data.username,
+						fontFamily = fontFamily,
+						fontSize = 15.sp,
+						modifier = Modifier
+							.padding(bottom = 7.dp)
+							.weight(1f)
+					)
+					if (action == "Messaging")
+						if (checked) Icon(
+							imageVector = ImageVector.vectorResource(id = R.drawable.ic_checkmark),
+							contentDescription = null,
+							tint = MaterialTheme.colors.primary,
+							modifier = Modifier
+								.width(30.dp)
+								.height(30.dp)
+								.padding(end = 10.dp)
+						)
+				}
+				Text(
+					text = data.profile.status ?: "",
+					fontFamily = fontFamily,
+					fontSize = 13.sp,
+					color = Color.Gray
+				)
+			}
+		}
+	}
 }
