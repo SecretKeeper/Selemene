ca1dbdbefee8b87b79de1efd88a9bcbde89fc315
Secret Keeper <shervin.ourfamily@gmail.com>
2022-05-25 02:28:15 +0430

    change API with new compose API version

--- app/src/main/java/net/teamof/whisper/components/messaging/BottomSheetGalleryTab.kt
+++ app/src/main/java/net/teamof/whisper/components/messaging/BottomSheetGalleryTab.kt
@@ -3,9 +3,9 @@ package net.teamof.whisper.components.messaging
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.lazy.GridCells
-import androidx.compose.foundation.lazy.LazyVerticalGrid
-import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.itemsIndexed
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
@@ -17,15 +17,15 @@ import coil.compose.AsyncImage
 @ExperimentalFoundationApi
 @Composable
 fun BottomSheetGalleryTab(images: List<GalleryImage>) {
-	LazyVerticalGrid(cells = GridCells.Adaptive(minSize = 130.dp)) {
+	LazyVerticalGrid(columns = GridCells.Adaptive(minSize = 130.dp)) {
 		itemsIndexed(images) { _, image ->
 			AsyncImage(
 				model = image.contentUri,
 				contentScale = ContentScale.FillBounds,
 				contentDescription = null,
 				modifier = Modifier
-                    .width(130.dp)
-                    .height(130.dp)
+					.width(130.dp)
+					.height(130.dp)
 			)
 		}
 	}
