45a6ce6225605e699ac97cebf2ab4607ff1bff77
Secret Keeper <shervin.ourfamily@gmail.com>
2022-05-25 02:29:21 +0430

    set maxLines 1 for status with overflow Ellipsis

--- app/src/main/java/net/teamof/whisper/components/Contact.kt
+++ app/src/main/java/net/teamof/whisper/components/Contact.kt
@@ -12,6 +12,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.vectorResource
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
@@ -85,7 +86,9 @@ fun Contact(
 					text = data.profile.status ?: "",
 					fontFamily = fontFamily,
 					fontSize = 13.sp,
-					color = Color.Gray
+					color = Color.Gray,
+					overflow = TextOverflow.Ellipsis,
+					maxLines = 1
 				)
 			}
 		}
