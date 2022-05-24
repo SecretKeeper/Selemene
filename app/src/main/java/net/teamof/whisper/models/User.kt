5e0c93e2d45c50c4380db8dc77ba000fb0d5600e
Secret Keeper <shervin.ourfamily@gmail.com>
2022-05-25 02:31:25 +0430

    make status and description required with default empty string

--- app/src/main/java/net/teamof/whisper/models/User.kt
+++ app/src/main/java/net/teamof/whisper/models/User.kt
@@ -10,29 +10,29 @@ import io.objectbox.relation.ToOne
 @Entity
 @JsonClass(generateAdapter = true)
 data class User(
-    @Id
-    var id: Long = 0,
-    @Index @Unique var user_id: Long = 0,
-    var username: String = "",
-    var email: String = "",
-    var avatar: String = ""
+	@Id
+	var id: Long = 0,
+	@Index @Unique var user_id: Long = 0,
+	var username: String = "",
+	var email: String = "",
+	var avatar: String = ""
 ) {
-    lateinit var profile: ToOne<Profile>
-    lateinit var counters: ToOne<Counters>
+	lateinit var profile: ToOne<Profile>
+	lateinit var counters: ToOne<Counters>
 }
 
 @Entity
 @JsonClass(generateAdapter = true)
 data class Profile(
-    @Id var id: Long = 0,
-    var description: String? = "",
-    var status: String? = ""
+	@Id var id: Long = 0,
+	var description: String = "",
+	var status: String = ""
 )
 
 @Entity
 @JsonClass(generateAdapter = true)
 data class Counters(
-    @Id var id: Long = 0,
-    var feeds: Int = 0,
-    var followers: Int = 0
+	@Id var id: Long = 0,
+	var feeds: Int = 0,
+	var followers: Int = 0
 )
