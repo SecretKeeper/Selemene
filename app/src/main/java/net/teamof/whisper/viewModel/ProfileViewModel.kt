181c9d853baf3f2460424a9f7f4595cf8f6a4720
Secret Keeper <shervin.ourfamily@gmail.com>
2022-05-25 02:30:30 +0430

    set also status after fetching user data

--- app/src/main/java/net/teamof/whisper/viewModel/ProfileViewModel.kt
+++ app/src/main/java/net/teamof/whisper/viewModel/ProfileViewModel.kt
@@ -16,97 +16,103 @@ import javax.inject.Inject
 
 @HiltViewModel
 class ProfileViewModel @Inject constructor(
-    private val usersAPI: UsersAPI
+	private val usersAPI: UsersAPI
 ) :
-    ViewModel() {
+	ViewModel() {
 
-    private val userBox: Box<User> = ObjectBox.store.boxFor()
+	private val userBox: Box<User> = ObjectBox.store.boxFor()
 
-    private val _userState: MutableLiveData<User> by lazy {
-        MutableLiveData<User>(null)
-    }
+	private val _userState: MutableLiveData<User> by lazy {
+		MutableLiveData<User>(null)
+	}
 
-    val userState: MutableLiveData<User> = _userState
+	val userState: MutableLiveData<User> = _userState
 
-    fun setUserStateByUserID(user_id: Long, callback: () -> Unit) {
-        val query = userBox.query().equal(User_.user_id, user_id)
-        val user = query.build().findFirst()
+	fun setUserStateByUserID(user_id: Long, callback: () -> Unit) {
+		val query = userBox.query().equal(User_.user_id, user_id)
+		val user = query.build().findFirst()
 
-        if (user == null) {
-            fetchUserByID(user_id) { fetchedUser ->
-                val newUser = User(
-                    user_id = fetchedUser.user_id,
-                    username = fetchedUser.username,
-                    email = fetchedUser.email,
-                    avatar = fetchedUser.avatar ?: "",
-                )
-                newUser.profile.target = Profile(description = fetchedUser.profile.description)
-                newUser.counters.target = Counters(
-                    followers = fetchedUser._count?.followers ?: 0,
-                    feeds = fetchedUser._count?.feeds ?: 0
-                )
+		if (user == null) {
+			fetchUserByID(user_id) { fetchedUser ->
+				val newUser = User(
+					user_id = fetchedUser.user_id,
+					username = fetchedUser.username,
+					email = fetchedUser.email,
+					avatar = fetchedUser.avatar ?: "",
+				)
+				newUser.profile.target = Profile(
+					status = fetchedUser.profile.status,
+					description = fetchedUser.profile.description
+				)
+				newUser.counters.target = Counters(
+					followers = fetchedUser._count?.followers ?: 0,
+					feeds = fetchedUser._count?.feeds ?: 0
+				)
 
-                userBox.put(newUser)
-                userState.value = newUser
-                callback()
-            }
+				userBox.put(newUser)
+				userState.value = newUser
+				callback()
+			}
 
-        } else {
-            _userState.value = user
-            callback()
-        }
-    }
+		} else {
+			_userState.value = user
+			callback()
+		}
+	}
 
-    fun getUserByUserID(user_id: Long): User? {
-        var userResult: User? = null
-        val query = userBox.query().equal(User_.user_id, user_id)
-        val user = query.build().findFirst()
+	fun getUserByUserID(user_id: Long): User? {
+		var userResult: User? = null
+		val query = userBox.query().equal(User_.user_id, user_id)
+		val user = query.build().findFirst()
 
-        if (user == null) {
-            fetchUserByID(user_id) { fetchedUser ->
-                val newUser = User(
-                    user_id = fetchedUser.user_id,
-                    username = fetchedUser.username,
-                    email = fetchedUser.email,
-                    avatar = fetchedUser.avatar ?: "",
-                )
-                newUser.profile.target = Profile(description = fetchedUser.profile.description)
-                newUser.counters.target = Counters(
-                    followers = fetchedUser._count?.followers ?: 0,
-                    feeds = fetchedUser._count?.feeds ?: 0
-                )
+		if (user == null) {
+			fetchUserByID(user_id) { fetchedUser ->
+				val newUser = User(
+					user_id = fetchedUser.user_id,
+					username = fetchedUser.username,
+					email = fetchedUser.email,
+					avatar = fetchedUser.avatar ?: "",
+				)
+				newUser.profile.target = Profile(
+					status = fetchedUser.profile.status,
+					description = fetchedUser.profile.description
+				)
+				newUser.counters.target = Counters(
+					followers = fetchedUser._count?.followers ?: 0,
+					feeds = fetchedUser._count?.feeds ?: 0
+				)
 
-                userBox.put(newUser)
-                userResult = newUser
-            }
+				userBox.put(newUser)
+				userResult = newUser
+			}
 
-        } else userResult = user
+		} else userResult = user
 
-        return userResult
-    }
+		return userResult
+	}
 
-    private fun fetchUserByID(user_id: Long, function: (UserAPI) -> Unit): UserAPI? {
-        val user: UserAPI? = null
-        val response = usersAPI.getUserProfile(user_id.toString())
+	private fun fetchUserByID(user_id: Long, function: (UserAPI) -> Unit): UserAPI? {
+		val user: UserAPI? = null
+		val response = usersAPI.getUserProfile(user_id.toString())
 
-        response.enqueue(object : Callback<UserAPI> {
-            override fun onResponse(
-                call: Call<UserAPI>,
-                response: Response<UserAPI>
-            ) {
-                Timber.e(response.toString())
-                response.body()?.let {
-                    function(it)
-                }
-            }
+		response.enqueue(object : Callback<UserAPI> {
+			override fun onResponse(
+				call: Call<UserAPI>,
+				response: Response<UserAPI>
+			) {
+				Timber.e(response.toString())
+				response.body()?.let {
+					function(it)
+				}
+			}
 
-            override fun onFailure(call: Call<UserAPI>, t: Throwable) {
-                Timber.e(t)
-            }
+			override fun onFailure(call: Call<UserAPI>, t: Throwable) {
+				Timber.e(t)
+			}
 
-        })
+		})
 
-        return user
-    }
+		return user
+	}
 }
 
