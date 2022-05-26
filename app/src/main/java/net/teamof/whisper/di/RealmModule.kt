161f8932ae958bcb3bbcebb9eebba151d02fc66f
Secret Keeper <shervin.ourfamily@gmail.com>
2022-05-26 20:57:18 +0430

    init

---
+++ app/src/main/java/net/teamof/whisper/di/RealmModule.kt
@@ -0,0 +1,33 @@
+package net.teamof.whisper.di
+
+import android.content.Context
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import io.realm.Realm
+import io.realm.RealmConfiguration
+import net.teamof.whisper.utils.KeyStoreUtils
+import javax.inject.Singleton
+
+@InstallIn(SingletonComponent::class)
+@Module
+object RealmModule {
+	@Provides
+	@Singleton
+	fun providesRealm(
+		@ApplicationContext context: Context,
+		realmKeyStore: KeyStoreUtils
+	): Realm {
+		Realm.init(context)
+		val realmConfiguration = RealmConfiguration
+			.Builder()
+			.name("RealmValkyrie")
+			.encryptionKey(realmKeyStore.getOrCreateEncryptionKey())
+			.deleteRealmIfMigrationNeeded()
+			.build()
+		Realm.setDefaultConfiguration(realmConfiguration)
+		return Realm.getDefaultInstance()
+	}
+}
