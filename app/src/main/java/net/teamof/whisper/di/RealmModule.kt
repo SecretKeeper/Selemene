e5c7cafc321733d76de5a5ce0e6b152fa344b905
Secret Keeper <shervin.ourfamily@gmail.com>
2022-05-26 21:26:31 +0430

    renamed KeyStoreUtils to RealmKeyStoreUtils

--- app/src/main/java/net/teamof/whisper/di/RealmModule.kt
+++ app/src/main/java/net/teamof/whisper/di/RealmModule.kt
@@ -8,7 +8,7 @@ import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
 import io.realm.Realm
 import io.realm.RealmConfiguration
-import net.teamof.whisper.utils.KeyStoreUtils
+import net.teamof.whisper.utils.RealmKeyStoreUtils
 import javax.inject.Singleton
 
 @InstallIn(SingletonComponent::class)
@@ -18,7 +18,7 @@ object RealmModule {
 	@Singleton
 	fun providesRealm(
 		@ApplicationContext context: Context,
-		realmKeyStore: KeyStoreUtils
+		realmKeyStore: RealmKeyStoreUtils
 	): Realm {
 		Realm.init(context)
 		val realmConfiguration = RealmConfiguration
