42e38eb4a4427c865dc5fa306085031a75e20874
SecretKeeper <shervin.ourfamily@gmail.com>
2022-05-27 19:49:57 +0430

    init

---
+++ app/src/main/java/net/teamof/whisper/data/AppDatabase.kt
@@ -0,0 +1,35 @@
+package net.teamof.whisper.data
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import net.teamof.whisper.utils.DATABASE_NAME
+
+/**
+ * The Room database for this app
+ */
+@Database(entities = [User::class, KeyValue::class], version = 1, exportSchema = false)
+abstract class AppDatabase : RoomDatabase() {
+	abstract fun userDAO(): UserDAO
+
+	abstract fun keyValueDao(): KeyValueDAO
+
+	companion object {
+
+		// For Singleton instantiation
+		@Volatile
+		private var instance: AppDatabase? = null
+
+		fun getInstance(context: Context): AppDatabase {
+			return instance ?: synchronized(this) {
+				instance ?: buildDatabase(context).also { instance = it }
+			}
+		}
+
+		private fun buildDatabase(context: Context): AppDatabase {
+			return Room.databaseBuilder(context, AppDatabase::class.java, DATABASE_NAME)
+				.build()
+		}
+	}
+}
