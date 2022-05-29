c0c25c2f0035ffca1f814b91054042a56d0c7c2f
SecretKeeper <shervin.ourfamily@gmail.com>
2022-05-30 01:10:49 +0430

    code refactoring with new Room DB API desing

--- app/src/main/java/net/teamof/whisper/screens/Messaging.kt
+++ app/src/main/java/net/teamof/whisper/screens/Messaging.kt
@@ -22,7 +22,7 @@ import net.teamof.whisper.components.Message
 import net.teamof.whisper.components.messaging.MessagingAttachSource
 import net.teamof.whisper.components.messaging.MessagingFooter
 import net.teamof.whisper.components.messaging.MessagingHeader
-import net.teamof.whisper.models.Message
+import net.teamof.whisper.data.Message
 import net.teamof.whisper.viewModel.MessagesViewModel
 import net.teamof.whisper.viewModel.ProfileViewModel
 
@@ -35,56 +35,59 @@ import net.teamof.whisper.viewModel.ProfileViewModel
 @SuppressLint("RememberReturnType")
 @Composable
 fun Messaging(
-    navController: NavController,
-    to_user_id: String,
-    messagesViewModel: MessagesViewModel,
-    currentUserId: Long,
-    profileViewModel: ProfileViewModel
+	navController: NavController,
+	to_user_id: String,
+	messagesViewModel: MessagesViewModel,
+	currentUserId: Long,
+	profileViewModel: ProfileViewModel
 ) {
 
-    messagesViewModel.getConversationMessages(to_user_id.toLong())
-    val messages: List<Message> by messagesViewModel.messages.observeAsState(listOf())
-    val bottomSheetState = rememberModalBottomSheetState(ModalBottomSheetValue.Hidden)
 
-    val scope = rememberCoroutineScope()
-    val selection = remember { mutableStateOf(false) }
-    if (selection.value || bottomSheetState.isVisible) {
-        BackPressHandler {
-            selection.value = false
-            scope.launch { bottomSheetState.hide() }
-        }
-    }
+	val messages: List<Message> by messagesViewModel.getConversationMessages(to_user_id.toLong())
+		.observeAsState(
+			listOf()
+		)
+	val bottomSheetState = rememberModalBottomSheetState(ModalBottomSheetValue.Hidden)
 
-    ModalBottomSheetLayout(
-        sheetContent = { MessagingAttachSource() },
-        sheetState = bottomSheetState
-    ) {
+	val scope = rememberCoroutineScope()
+	val selection = remember { mutableStateOf(false) }
+	if (selection.value || bottomSheetState.isVisible) {
+		BackPressHandler {
+			selection.value = false
+			scope.launch { bottomSheetState.hide() }
+		}
+	}
 
-        Column {
-            MessagingHeader(
-                navController,
-                profileViewModel,
-                to_user_id.toLong(),
-                selection
-            )
-            Column(Modifier.weight(1f)) {
-                Column(
-                    Modifier.verticalScroll(
-                        state = rememberScrollState(),
-                        reverseScrolling = true
-                    )
-                ) {
-                    messages.forEach { message ->
-                        Message(
-                            currentUserId,
-                            message,
-                            selection.value,
-                            enableSelectionMode = { selection.value = true },
-                        )
-                    }
-                }
-            }
-            MessagingFooter(bottomSheetState, messagesViewModel, currentUserId, to_user_id.toLong())
-        }
-    }
+	ModalBottomSheetLayout(
+		sheetContent = { MessagingAttachSource() },
+		sheetState = bottomSheetState
+	) {
+
+		Column {
+			MessagingHeader(
+				navController,
+				profileViewModel,
+				to_user_id.toLong(),
+				selection
+			)
+			Column(Modifier.weight(1f)) {
+				Column(
+					Modifier.verticalScroll(
+						state = rememberScrollState(),
+						reverseScrolling = true
+					)
+				) {
+					messages.forEach { message ->
+						Message(
+							currentUserId,
+							message,
+							selection.value,
+							enableSelectionMode = { selection.value = true },
+						)
+					}
+				}
+			}
+			MessagingFooter(bottomSheetState, messagesViewModel, currentUserId, to_user_id.toLong())
+		}
+	}
 }
