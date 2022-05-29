572330d6002fadf5b8842844d25cd7901fda0e66
SecretKeeper <shervin.ourfamily@gmail.com>
2022-05-30 01:05:04 +0430

    init

---
+++ app/src/main/java/net/teamof/whisper/data/MessageDAO.kt.kt
@@ -0,0 +1,30 @@
+package net.teamof.whisper.data
+
+import androidx.lifecycle.LiveData
+import androidx.room.*
+
+
+@Dao
+abstract class MessageDAO : BaseDao<Message>() {
+
+	@Query("SELECT * FROM messages")
+	abstract fun getAllMessages(): LiveData<List<Message>>
+
+	@Query("SELECT * FROM messages WHERE target_user = :targetUserId OR user_id = :targetUserId ORDER BY created_at")
+	abstract fun getMessagesByTargetUserId(targetUserId: Long): LiveData<List<Message>>
+
+	@Insert(onConflict = OnConflictStrategy.IGNORE)
+	abstract override fun insert(obj: Message): Long
+
+	@Insert(onConflict = OnConflictStrategy.IGNORE)
+	abstract override fun insert(obj: MutableList<Message>?): MutableList<Long>
+
+	@Update
+	abstract override fun update(obj: Message)
+
+	@Update
+	abstract override fun update(obj: MutableList<Message>?)
+
+	@Delete
+	abstract override fun delete(obj: Message)
+}
