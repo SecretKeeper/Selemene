c2240ef66ac8d1ded05fdeb933a9b0328e306449
SecretKeeper <shervin.ourfamily@gmail.com>
2022-05-30 01:07:25 +0430

    refactoring with new Room DB API

--- app/src/main/java/net/teamof/whisper/viewModel/ConversationActionsViewModel.kt
+++ app/src/main/java/net/teamof/whisper/viewModel/ConversationActionsViewModel.kt
@@ -3,58 +3,58 @@ package net.teamof.whisper.viewModel
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
-import net.teamof.whisper.repositories.ConversationRepository
+import net.teamof.whisper.data.ConversationRepository
 import javax.inject.Inject
 
 @HiltViewModel
 class ConversationActionsViewModel @Inject constructor(
-    private val conversationRepository: ConversationRepository
+	private val conversationRepository: ConversationRepository
 ) : ViewModel() {
 
-    private val _showActionsState: MutableLiveData<Boolean> by lazy {
-        MutableLiveData<Boolean>(false)
-    }
+	private val _showActionsState: MutableLiveData<Boolean> by lazy {
+		MutableLiveData<Boolean>(false)
+	}
 
-    val showActionsState: MutableLiveData<Boolean> = _showActionsState
+	val showActionsState: MutableLiveData<Boolean> = _showActionsState
 
-    private val _selectedConversations: MutableLiveData<List<Long>> by lazy {
-        MutableLiveData<List<Long>>(mutableListOf())
-    }
+	private val _selectedConversations: MutableLiveData<List<Long>> by lazy {
+		MutableLiveData<List<Long>>(mutableListOf())
+	}
 
-    val selectedConversations: MutableLiveData<List<Long>> = _selectedConversations
+	val selectedConversations: MutableLiveData<List<Long>> = _selectedConversations
 
 
-    fun showActions(conversation_id: Long) {
-        _selectedConversations.value = (_selectedConversations.value)?.let {
-            mutableListOf(
-                *it.toTypedArray(),
-                conversation_id
-            )
-        }
-        _showActionsState.value = true
-    }
+	fun showActions(conversation_id: Long) {
+		_selectedConversations.value = (_selectedConversations.value)?.let {
+			mutableListOf(
+				*it.toTypedArray(),
+				conversation_id
+			)
+		}
+		_showActionsState.value = true
+	}
 
-    fun hideActions() {
-        _selectedConversations.value = mutableListOf()
-        _showActionsState.value = false
-    }
+	fun hideActions() {
+		_selectedConversations.value = mutableListOf()
+		_showActionsState.value = false
+	}
 
-    fun selectConversationByToUserID(user_id: Long) {
-        _selectedConversations.value = (_selectedConversations.value)?.let {
-            mutableListOf(
-                *it.toTypedArray(),
-                user_id
-            )
-        }
-    }
+	fun selectConversationByToUserID(user_id: Long) {
+		_selectedConversations.value = (_selectedConversations.value)?.let {
+			mutableListOf(
+				*it.toTypedArray(),
+				user_id
+			)
+		}
+	}
 
-    fun unselectConversationByToUserID(user_id: Long) {
-        _selectedConversations.value =
-            _selectedConversations.value?.filter { it != user_id }
-    }
+	fun unselectConversationByToUserID(user_id: Long) {
+		_selectedConversations.value =
+			_selectedConversations.value?.filter { it != user_id }
+	}
 
-    fun deleteSelectedConversations() {
-        if (_selectedConversations.value != null)
-            conversationRepository.deleteByToUserIDs(_selectedConversations.value!!)
-    }
+	fun deleteSelectedConversations() {
+		if (_selectedConversations.value != null)
+			conversationRepository.deleteByUserIds(_selectedConversations.value!!.toMutableList())
+	}
 }
