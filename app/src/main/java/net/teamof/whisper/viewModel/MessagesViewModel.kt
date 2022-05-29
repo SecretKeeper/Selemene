e6d972fb8f783501308b6990490d6e843f3e88ae
SecretKeeper <shervin.ourfamily@gmail.com>
2022-05-30 01:05:53 +0430

    refactoring with new Room DB API

--- app/src/main/java/net/teamof/whisper/viewModel/MessagesViewModel.kt
+++ app/src/main/java/net/teamof/whisper/viewModel/MessagesViewModel.kt
@@ -5,23 +5,18 @@ import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import android.content.IntentFilter
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
-import io.objectbox.Box
-import io.objectbox.android.AndroidScheduler
-import io.objectbox.android.ObjectBoxLiveData
-import io.objectbox.kotlin.equal
-import io.objectbox.kotlin.or
-import io.objectbox.query.Query
-import net.teamof.whisper.ObjectBox
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import net.teamof.whisper.data.ConversationRepository
+import net.teamof.whisper.data.Message
+import net.teamof.whisper.data.MessageRepository
 import net.teamof.whisper.models.DeliveryReport
-import net.teamof.whisper.models.Message
 import net.teamof.whisper.models.MessageSide
-import net.teamof.whisper.models.Message_
-import net.teamof.whisper.repositories.ConversationRepository
-import net.teamof.whisper.repositories.MessageRepository
 import net.teamof.whisper.sharedprefrences.SharedPreferencesManagerImpl
-import timber.log.Timber
 import javax.inject.Inject
 
 @HiltViewModel
@@ -40,8 +35,13 @@ class MessagesViewModel @Inject constructor(
 				intent.getSerializableExtra("RECEIVE_ASSIGNED_MESSAGE") as? Message
 
 			if (message != null) {
-				conversationRepository.update(MessageSide.THEMSELVES, message)
-				messageRepository.create(message)
+				CoroutineScope(Dispatchers.IO).launch {
+					conversationRepository.updateConversationByReceivingMessage(
+						MessageSide.THEMSELVES,
+						message
+					)
+					messageRepository.upsert(message)
+				}
 				application.sendBroadcast(
 					Intent("SEND_DELIVERY_REPORT").putExtra(
 						"DELIVERY_REPORT_MODEL",
@@ -50,7 +50,7 @@ class MessagesViewModel @Inject constructor(
 				)
 			}
 
-			if (assignedMessage != null) messageRepository.updateAssignedMessage(assignedMessage)
+			if (assignedMessage != null) messageRepository.upsert(assignedMessage)
 
 		}
 	}
@@ -62,33 +62,17 @@ class MessagesViewModel @Inject constructor(
 		)
 	}
 
-	private val messageBox: Box<Message> = ObjectBox.store.boxFor(Message::class.java)
-
 	private val currentUserId = getCurrentUserId()
 
 	private fun getCurrentUserId(): Long =
 		sharedPreferencesManagerImpl.getLong("userId", 0L)
 
-	private var _messages: ObjectBoxLiveData<Message> =
-		ObjectBoxLiveData(fetchAndObserveMessages())
-
-	val messages: ObjectBoxLiveData<Message> = _messages
-
-	private fun fetchAndObserveMessages(): Query<Message>? {
-		val query = messageBox.query().build()
-
-		query.subscribe().on(AndroidScheduler.mainThread()).observer {
-			Timber.d("QQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQ ")
-		}
-
-		return query
-	}
-
 	fun sendMessage(message: Message) {
 
-		messageRepository.create(message)
-
-		conversationRepository.update(MessageSide.MYSELF, message)
+		CoroutineScope(Dispatchers.IO).launch {
+			messageRepository.insert(message)
+			conversationRepository.updateConversationByReceivingMessage(MessageSide.MYSELF, message)
+		}
 
 		application.sendBroadcast(
 			Intent("SEND_MESSAGE").putExtra(
@@ -98,14 +82,7 @@ class MessagesViewModel @Inject constructor(
 		)
 	}
 
-	fun getConversationMessages(to_user_id: Long) {
-		_messages = ObjectBoxLiveData(
-			messageBox.query().run {
-				(Message_.content equal to_user_id
-						or (Message_.user_id equal currentUserId))
-				order(Message_.id)
-				build()
-			}
-		)
-	}
+	fun getConversationMessages(targetUserId: Long): LiveData<List<Message>> =
+		messageRepository.getConversationMessages(targetUserId)
+
 }
