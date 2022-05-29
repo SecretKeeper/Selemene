f05681dd46bfd73d2f222c6dec8635caea5b5957
SecretKeeper <shervin.ourfamily@gmail.com>
2022-05-29 17:49:59 +0430

    added Converters and Conversation model

--- app/src/main/java/net/teamof/whisper/data/AppDatabase.kt
+++ app/src/main/java/net/teamof/whisper/data/AppDatabase.kt
@@ -4,16 +4,24 @@ import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
 import net.teamof.whisper.utils.DATABASE_NAME
 
 /**
  * The Room database for this app
  */
-@Database(entities = [User::class, KeyValue::class], version = 1, exportSchema = false)
+@Database(
+	entities = [User::class, KeyValue::class, Conversation::class],
+	version = 1,
+	exportSchema = false
+)
+@TypeConverters(Converters::class)
 abstract class AppDatabase : RoomDatabase() {
 	abstract fun userDAO(): UserDAO
 
 	abstract fun keyValueDao(): KeyValueDAO
+
+	abstract fun conversationDao(): ConversationDAO
 
 	companion object {
 
