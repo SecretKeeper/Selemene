c01c865d466889f39e66328e07766ae88a981076
SecretKeeper <shervin.ourfamily@gmail.com>
2022-06-08 21:30:17 +0430

    added feeds follower and changed some styles

--- app/src/main/java/net/teamof/whisper/screens/SelfProfile.kt
+++ app/src/main/java/net/teamof/whisper/screens/SelfProfile.kt
@@ -9,14 +9,15 @@ import androidx.activity.result.contract.ActivityResultContracts
 import androidx.annotation.RequiresApi
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.AlertDialog
-import androidx.compose.material.Text
-import androidx.compose.material.TextButton
+import androidx.compose.foundation.text.ClickableText
+import androidx.compose.material.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.AnnotatedString
+import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
@@ -36,183 +37,219 @@ import net.teamof.whisper.viewModel.UserViewModel
 @Composable
 fun SelfProfile(navController: NavController, userViewModel: UserViewModel) {
 
-	val context = LocalContext.current
-	val openDialog = remember { mutableStateOf(false) }
+    val context = LocalContext.current
+    val openDialog = remember { mutableStateOf(false) }
 
-	val showToast = Toast.makeText(context, "This feature is not available yet", Toast.LENGTH_SHORT)
-	val username = userViewModel.getUsername()
-	val avatar = remember { mutableStateOf(userViewModel.getAvatar()) }
+    val showToast = Toast.makeText(context, "This feature is not available yet", Toast.LENGTH_SHORT)
+    val username = userViewModel.getUsername()
+    val avatar = remember { mutableStateOf(userViewModel.getAvatar()) }
 
-	var imageUri by remember {
-		mutableStateOf<Uri?>(null)
-	}
+    var imageUri by remember {
+        mutableStateOf<Uri?>(null)
+    }
 
-	val bitmap = remember {
-		mutableStateOf<Bitmap?>(null)
-	}
+    val bitmap = remember {
+        mutableStateOf<Bitmap?>(null)
+    }
 
-	val imageCropLauncher = rememberLauncherForActivityResult(CropImageContract()) { result ->
-		if (result.isSuccessful) {
-			imageUri = result.uriContent
-			imageUri?.let { userViewModel.setAvatar(it) }
-		} else {
-			val exception = result.error
-		}
-	}
+    val imageCropLauncher = rememberLauncherForActivityResult(CropImageContract()) { result ->
+        if (result.isSuccessful) {
+            imageUri = result.uriContent
+            imageUri?.let { userViewModel.setAvatar(it) }
+        } else {
+            val exception = result.error
+        }
+    }
 
-	val imagePickerLauncher =
-		rememberLauncherForActivityResult(contract = ActivityResultContracts.GetContent()) { uri: Uri? ->
-			val cropOptions = CropImageContractOptions(
-				uri,
-				CropImageOptions()
-			)
-				.setImageSource(includeCamera = false, includeGallery = true)
-				.setGuidelines(CropImageView.Guidelines.ON)
-				.setCropCornerShape(cornerShape = CropImageView.CropCornerShape.RECTANGLE)
-				.setAspectRatio(4, 5)
+    val imagePickerLauncher =
+        rememberLauncherForActivityResult(contract = ActivityResultContracts.GetContent()) { uri: Uri? ->
+            val cropOptions = CropImageContractOptions(
+                uri,
+                CropImageOptions()
+            )
+                .setImageSource(includeCamera = false, includeGallery = true)
+                .setGuidelines(CropImageView.Guidelines.ON)
+                .setCropCornerShape(cornerShape = CropImageView.CropCornerShape.RECTANGLE)
+                .setAspectRatio(4, 5)
 
-			imageCropLauncher.launch(
-				cropOptions
-			)
-		}
+            imageCropLauncher.launch(
+                cropOptions
+            )
+        }
 
-	Column {
-		Column(modifier = Modifier.padding(top = 30.dp, start = 20.dp, end = 20.dp)) {
-			Row(
-				verticalAlignment = Alignment.CenterVertically,
-				modifier = Modifier.padding(bottom = 15.dp)
-			) {
-				Avatar(
-					if (imageUri != null)
-						Uri.parse(imageUri.toString()).toString()
-					else
-						avatar.value,
-					username,
-					90,
-					90
-				) { openDialog.value = true }
+    Column {
+        Column(modifier = Modifier.padding(top = 30.dp, start = 20.dp, end = 20.dp)) {
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
+                modifier = Modifier.padding(bottom = 15.dp)
+            ) {
+                Avatar(
+                    if (imageUri != null)
+                        Uri.parse(imageUri.toString()).toString()
+                    else
+                        avatar.value,
+                    username,
+                    90,
+                    90
+                ) { openDialog.value = true }
 
-				if (openDialog.value) {
-					AlertDialog(
-						onDismissRequest = {
-							openDialog.value = false
-						},
-						title = {
-							Text(text = "Avatar")
-						},
-						text = {
-							Text(
-								"which presents the details regarding the Dialog's purpose.",
-								fontSize = 14.sp,
-								fontFamily = fontFamily,
-								fontWeight = FontWeight.Normal,
-								lineHeight = 22.sp,
-							)
-						},
-						confirmButton = {
-							TextButton(
-								onClick = {
-									avatar.value = ""
-									userViewModel.removeAvatar()
-									openDialog.value = false
-								}
-							) {
-								Text(
-									"Remove",
-									color = RedAlertColor,
-									fontSize = 14.sp,
-									fontFamily = fontFamily,
-									fontWeight = FontWeight.SemiBold,
-									letterSpacing = 0.sp
-								)
-							}
-						},
-						dismissButton = {
-							TextButton(
-								onClick = {
-									imagePickerLauncher.launch("image/*")
-									openDialog.value = false
-								}
-							) {
-								Text(
-									"Set Image",
-									fontSize = 14.sp,
-									fontFamily = fontFamily,
-									fontWeight = FontWeight.SemiBold,
-									letterSpacing = 0.sp
-								)
-							}
-						}
-					)
-				}
+                if (openDialog.value) {
+                    AlertDialog(
+                        onDismissRequest = {
+                            openDialog.value = false
+                        },
+                        title = {
+                            Text(text = "Avatar")
+                        },
+                        text = {
+                            Text(
+                                "which presents the details regarding the Dialog's purpose.",
+                                fontSize = 14.sp,
+                                fontFamily = fontFamily,
+                                fontWeight = FontWeight.Normal,
+                                lineHeight = 22.sp,
+                            )
+                        },
+                        confirmButton = {
+                            TextButton(
+                                onClick = {
+                                    avatar.value = ""
+                                    userViewModel.removeAvatar()
+                                    openDialog.value = false
+                                }
+                            ) {
+                                Text(
+                                    "Remove",
+                                    color = RedAlertColor,
+                                    fontSize = 14.sp,
+                                    fontFamily = fontFamily,
+                                    fontWeight = FontWeight.SemiBold,
+                                    letterSpacing = 0.sp
+                                )
+                            }
+                        },
+                        dismissButton = {
+                            TextButton(
+                                onClick = {
+                                    imagePickerLauncher.launch("image/*")
+                                    openDialog.value = false
+                                }
+                            ) {
+                                Text(
+                                    "Set Image",
+                                    fontSize = 14.sp,
+                                    fontFamily = fontFamily,
+                                    fontWeight = FontWeight.SemiBold,
+                                    letterSpacing = 0.sp
+                                )
+                            }
+                        }
+                    )
+                }
 
-				Column(modifier = Modifier.padding(start = 20.dp)) {
-					Text(
-						text = username,
-						fontFamily = fontFamily,
-						fontWeight = FontWeight.Bold,
-						fontSize = 18.sp,
-						modifier = Modifier.padding(bottom = 10.dp)
-					)
-					Text(
-						text = userViewModel.getStatus(),
-						fontFamily = fontFamily,
-						fontWeight = FontWeight.Normal,
-						fontSize = 16.sp,
-						overflow = TextOverflow.Ellipsis,
-						maxLines = 1
-					)
-				}
-			}
-		}
+                Column(modifier = Modifier.padding(start = 20.dp)) {
+                    Text(
+                        text = username,
+                        fontFamily = fontFamily,
+                        fontWeight = FontWeight.Bold,
+                        fontSize = 18.sp,
+                        modifier = Modifier.padding(bottom = 9.dp)
+                    )
+                    Text(
+                        text = userViewModel.getStatus(),
+                        fontFamily = fontFamily,
+                        fontWeight = FontWeight.SemiBold,
+                        fontSize = 14.sp,
+                        overflow = TextOverflow.Ellipsis,
+                        color = Color(red = 23, green = 155, blue = 128),
+                        maxLines = 1,
+                        modifier = Modifier.padding(bottom = 9.dp)
+                    )
+                    Row {
+                        ClickableText(
+                            text = AnnotatedString(
+                                "109 Feeds",
+                                spanStyle = SpanStyle(
+                                    fontSize = 14.sp,
+                                    fontFamily = fontFamily,
+                                    fontWeight = FontWeight.SemiBold,
+                                    color = MaterialTheme.colors.primary
+                                )
+                            ),
+                            onClick = {},
+                            modifier = Modifier.padding(vertical = 0.dp)
+                        )
+                        Divider(
+                            modifier = Modifier
+                                .height(10.dp)
+                                .width(10.dp)
+                                .padding(horizontal = 15.dp)
+                        )
+                        ClickableText(
+                            text = AnnotatedString(
+                                "742 Followers",
+                                spanStyle = SpanStyle(
+                                    fontSize = 14.sp,
+                                    fontFamily = fontFamily,
+                                    fontWeight = FontWeight.SemiBold,
+                                    color = MaterialTheme.colors.primary
+                                )
+                            ),
+                            onClick = {},
+                            modifier = Modifier.padding(vertical = 0.dp)
+                        )
+                    }
+                }
+            }
+        }
 
-		Column(modifier = Modifier.padding(top = 30.dp, start = 20.dp, end = 20.dp)) {
-			SettingsItem(title = "My Account", event = { navController.navigate("MyAccount") })
-		}
+        Column(modifier = Modifier.padding(top = 30.dp, start = 20.dp, end = 20.dp)) {
+            SettingsItem(title = "My Account", event = { navController.navigate("MyAccount") })
+        }
 
-		Box(
-			modifier = Modifier
-				.fillMaxWidth()
-				.background(Color(red = 235, green = 235, blue = 235))
-				.height(1.dp)
-		)
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .background(Color(red = 235, green = 235, blue = 235))
+                .height(1.dp)
+        )
 
-		Column(modifier = Modifier.padding(top = 30.dp, start = 20.dp, end = 20.dp)) {
-			SettingsItem(title = "Notifications", event = { showToast.show() })
-			SettingsItem(title = "Privacy", event = { showToast.show() })
-			SettingsItem(title = "Security", event = { navController.navigate("Security") })
-			SettingsItem(title = "Devices", event = { showToast.show() })
-		}
+        Column(modifier = Modifier.padding(top = 30.dp, start = 20.dp, end = 20.dp)) {
+            SettingsItem(title = "Notifications", event = { showToast.show() })
+            SettingsItem(title = "Privacy", event = { showToast.show() })
+            SettingsItem(title = "Security", event = { navController.navigate("Security") })
+            SettingsItem(title = "Devices", event = { showToast.show() })
+        }
 
-		Box(
-			modifier = Modifier
-				.fillMaxWidth()
-				.background(Color(red = 235, green = 235, blue = 235))
-				.height(1.dp)
-		)
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .background(Color(red = 235, green = 235, blue = 235))
+                .height(1.dp)
+        )
 
-		Column(modifier = Modifier.padding(top = 30.dp, start = 20.dp, end = 20.dp)) {
-			SettingsItem(title = "FAQs", event = { showToast.show() })
-			SettingsItem(title = "Privacy & Policy", event = { showToast.show() })
-			SettingsItem(title = "Contact Support", event = { showToast.show() })
-		}
+        Column(modifier = Modifier.padding(top = 30.dp, start = 20.dp, end = 20.dp)) {
+            SettingsItem(title = "FAQs", event = { showToast.show() })
+            SettingsItem(title = "Privacy & Policy", event = { showToast.show() })
+            SettingsItem(title = "Contact Support", event = { showToast.show() })
+        }
 
-		Box(
-			modifier = Modifier
-				.fillMaxWidth()
-				.background(Color(red = 235, green = 235, blue = 235))
-				.height(1.dp)
-		)
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .background(Color(red = 235, green = 235, blue = 235))
+                .height(1.dp)
+        )
 
-		Column(modifier = Modifier.padding(top = 20.dp, start = 20.dp, end = 20.dp)) {
-			Text(
-				text = "Whispers - Version 1.0.0-alpha",
-				fontFamily = fontFamily,
-				fontWeight = FontWeight.Medium,
-				fontSize = 14.sp,
-				color = Color.Gray,
-				modifier = Modifier.weight(1f)
-			)
-		}
-	}
+        Column(modifier = Modifier.padding(top = 20.dp, start = 20.dp, end = 20.dp)) {
+            Text(
+                text = "Whispers - Version 1.0.0-alpha",
+                fontFamily = fontFamily,
+                fontWeight = FontWeight.Medium,
+                fontSize = 14.sp,
+                color = Color.Gray,
+                modifier = Modifier.weight(1f)
+            )
+        }
+    }
 }
